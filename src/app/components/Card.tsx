72a02443da14300ce214bb7efc8264525c7e7f59
Seokyung <xuejd17@gmail.com>
2025-05-20 14:41:40 +0900

    Card 컴포넌트 기능 추가

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,13 @@
+const Card = () => (
+  <div>
+    <h2>title</h2>
+    <span>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat corporis
+      explicabo quae maxime enim eius ipsa perspiciatis minus animi deleniti
+      velit eligendi, doloremque atque neque mollitia quisquam nesciunt quasi
+      laudantium?
+    </span>
+  </div>
+);
+
+export default Card;
