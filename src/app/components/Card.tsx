d35a9c8f297a0fb8d7276f74ed776d17786288b1
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2025-05-20 14:45:48 +0900

    Feature/issue 009/card (#13)
    * Card 컴포넌트 기능 추가
    
    * Card 컴포넌트에 버튼 추가

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,14 @@
+const Card = () => (
+  <div>
+    <h2>title</h2>
+    <span>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat corporis
+      explicabo quae maxime enim eius ipsa perspiciatis minus animi deleniti
+      velit eligendi, doloremque atque neque mollitia quisquam nesciunt quasi
+      laudantium?
+    </span>
+    <button>View All</button>
+  </div>
+);
+
+export default Card;
