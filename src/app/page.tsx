754d5fe9906d2f5ebe43be18eb0592caf02cb467
chanho <lch0309k@gmail.com>
2025-05-23 14:04:30 +0900

    feat: Poster 기본 props 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-const Home = () => <div>hello</div>;
+import Poster from "@/components/poster/Poster";
+
+const Home = () => {
+    return (
+      <div className='p-8'>
+        <Poster
+          src='https://picsum.photos/300/450'
+          className='mb-4'
+        />
+
+        <Poster
+          src='https://cdn.imweb.me/upload/S20200106a105fd03f4b57/74acf364c7b51.jpg'
+          shadow={false}
+          className='h-60 w-40'
+        />
+      </div>
+    );
+    
+}
 
 export default Home;
--- src/components/poster/Poster.tsx
+++ src/components/poster/Poster.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import Image from "next/image";
 
 interface PosterProps {
@@ -6,22 +7,31 @@ interface PosterProps {
   width?: string | number;
   height?: string | number;
   shadow?: boolean;
+  border?: boolean;
   className?: string;
 }
 
 const Poster = ({
   src,
   alt = '포스터 이미지',
-  width = '100%',
-  height = 'auto',
   shadow = true,
+  border = true,
   className,
 }: PosterProps) => {
   return (
     <Image
       src={src}
       alt={alt}
-      style={{ width, height }}
-      className="b"
-    )
-}
+      width={160}
+      height={240}
+      className={clsx(
+        'object-contain',
+        shadow && 'shadow-md',
+        border && 'border border-gray-300',
+        className
+      )}
+    />
+  );
+};
+
+export default Poster;
