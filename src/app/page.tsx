6662e977efd5d76c2114d6d829f4227fab084f97
chanho <lch0309k@gmail.com>
2025-05-23 15:07:18 +0900

    chore: page.tsx 파일 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import Poster from "@/components/poster/Poster";
-
-const Home = () => {
-    return (
-      <div className='p-8'>
-        <Poster
-          src='https://picsum.photos/300/450'
-        />
-
-        <Poster
-          src='https://cdn.imweb.me/upload/S20200106a105fd03f4b57/74acf364c7b51.jpg'
-                shadow={false}
-                size="xl"
-        />
-      </div>
-    );
-    
-}
+const Home = () => <div>hello</div>;
 
 export default Home;
