6ec4a809502a3305bda8782df72f78fcfd2a703c
Seokyung <xuejd17@gmail.com>
2025-05-23 14:32:44 +0900

    feat: Button 컴포넌트 props에 size, variant, disabled 추가

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,35 +2,56 @@ import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import { cn } from '@/lib/utils';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'sm' | 'md' | 'lg';
+  variant?: 'primary' | 'secondary' | 'danger' | 'outlined';
+  size?: 'sm' | 'md' | 'lg';
+  disabled?: boolean;
   className?: string;
   onClick?: () => void;
 }
 
 const Button = ({
-  variant = 'md',
+  variant = 'primary',
+  size = 'md',
+  disabled,
   className,
   onClick,
   children,
   ...props
 }: PropsWithChildren<ButtonProps>) => {
+  // Todo: 디자인 받은 후 스타일 변경 필요
   const buttonClasses = cn(
     // default style
-    'cursor-pointer',
+    'cursor-pointer rounded-sm inline-flex justify-center align-center',
 
-    // variant style (변경 필요)
+    // variant style
     {
-      'p-1': variant === 'sm',
-      'p-2': variant === 'md',
-      'p-3': variant === 'lg',
+      'bg-blue-500': variant === 'primary',
+      'bg-gray-500': variant === 'secondary',
+      'bg-red-500': variant === 'danger',
+      'border-2 border-gray-500': variant === 'outlined',
     },
+
+    // size style
+    {
+      'px-2 py-1': size === 'sm',
+      'px-5 py-1.5': size === 'md',
+      'px-8 py-2': size === 'lg',
+    },
+
+    // disabled style
+    disabled && 'cursor-not-allowed bg-gray-200 text-gray-400',
 
     // custom style
     className
   );
 
   return (
-    <button className={buttonClasses} onClick={onClick} {...props}>
+    <button
+      className={buttonClasses}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
       {children}
     </button>
   );
