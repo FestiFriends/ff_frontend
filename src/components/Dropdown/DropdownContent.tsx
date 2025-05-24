32795d73a9e09ec97233ad5828e2c9d276fca34c
Seokyung <xuejd17@gmail.com>
2025-05-25 01:34:33 +0900

    feat: DropdownContent 컴포넌트에 className props 추가

--- src/components/Dropdown/DropdownContent.tsx
+++ src/components/Dropdown/DropdownContent.tsx
@@ -1,13 +1,30 @@
 'use client';
 
 import { PropsWithChildren } from 'react';
+import { cn } from '@/lib/utils';
 import { useDropdownContext } from './DropdownContext';
 
-const DropdownContent = ({ children }: PropsWithChildren) => {
+interface DropdownContentProps {
+  className?: string;
+}
+
+const DropdownContent = ({
+  className,
+  children,
+}: PropsWithChildren<DropdownContentProps>) => {
   const { isOpen } = useDropdownContext();
+
   if (!isOpen) return null;
 
-  return <ul className='absolute z-10'>{children}</ul>;
+  const dropdownContentClasses = cn(
+    // default style
+    'absolute z-10',
+
+    // custom style
+    className
+  );
+
+  return <ul className={dropdownContentClasses}>{children}</ul>;
 };
 
 export default DropdownContent;
