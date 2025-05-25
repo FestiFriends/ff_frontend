c1a1bc6ad6e69da517318a2f0d73e384f0ba0cb7
chanho <lch0309k@gmail.com>
2025-05-25 16:12:05 +0900

    feat: useInput 훅 onBlur 추가

--- src/app/hooks/useInput.ts
+++ src/app/hooks/useInput.ts
@@ -4,14 +4,22 @@ import { useState, ChangeEvent } from 'react';
 
 const useInput = (initialValue = '') => {
   const [value, setValue] = useState(initialValue);
+  const [touched, setTouched] = useState(false);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  const reset = () => setValue('');
+  const onBlur = () => {
+    setTouched(true);
+  };
 
-  return { value, onChange, setValue, reset };
+  const reset = () => {
+    setValue('');
+    setTouched(false);
+  };
+
+  return { value, onChange, onBlur, setValue, reset, touched };
 };
 
 export default useInput;
