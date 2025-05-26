2d880c04e0c890d861478244e51c5df064623b59
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2025-05-26 18:47:13 +0900

    [Feature/issue-064] SearchInput 컴포넌트 구현 및 테스트 코드 작성, 문서화 (#86)
    * feat: SearchInput 컴포넌트 추가
    
    - form 제출 시 onSubmit 콜백 호출
    - input props 확장 및 ref 지원
    - 검색 버튼과 input 필드 포함한 폼 구현
    
    * feat: input type으로 search추가
    
    * chore: userEvent 의존성 설치
    
    * test: SearchInput 테스트코드 작성
    
    * docs: SearchInput컴포넌트 문서 작성

---
+++ src/components/SearchInput/SearchInput.test.tsx
@@ -0,0 +1,70 @@
+import { createRef, useState } from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SearchInput from './SearchInput';
+
+describe('<SearchInput>컴포넌트 테스트', () => {
+  test('input과 버튼이 렌더링되고, 버튼 텍스트가 표시된다', () => {
+    render(<SearchInput onSubmit={() => {}}>제출</SearchInput>);
+
+    const input = screen.getByLabelText('검색어 입력');
+    const button = screen.getByRole('button', { name: '제출' });
+
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  test('input에 ref가 정상적으로 전달된다', async () => {
+    const inputRef = createRef<HTMLInputElement | null>();
+    render(
+      <SearchInput
+        onSubmit={() => {}}
+        ref={inputRef}
+      >
+        제출
+      </SearchInput>
+    );
+
+    const input = screen.getByLabelText('검색어 입력');
+
+    await userEvent.type(input, '검색');
+    expect(inputRef.current?.value).toBe('검색');
+  });
+
+  test('input에 value와 onChange가 정상적으로 전달된다', async () => {
+    const TestComponent = () => {
+      const [value, setValue] = useState('');
+      return (
+        <SearchInput
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onSubmit={() => {}}
+        >
+          제출
+        </SearchInput>
+      );
+    };
+
+    render(<TestComponent />);
+
+    const input = screen.getByLabelText('검색어 입력');
+
+    await userEvent.type(input, '검색');
+    expect(input).toHaveValue('검색');
+  });
+
+  test('버튼 클릭, 엔터 입력시 폼이 제출된다', async () => {
+    const onSubmit = jest.fn();
+    render(<SearchInput onSubmit={onSubmit}>제출</SearchInput>);
+
+    const input = screen.getByLabelText('검색어 입력');
+    const button = screen.getByRole('button', { name: '제출' });
+
+    input.focus();
+    await userEvent.keyboard('{Enter}');
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+
+    await userEvent.click(button);
+    expect(onSubmit).toHaveBeenCalledTimes(2);
+  });
+});
