6c18b4760ab8d271c318bfb4383f7b73b1a2120a
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2025-05-26 09:45:37 +0900

    [Feature/issue-051] Dropdown 컴포넌트 구현 (#81)
    * feat: Dropdown 컴포넌트 구현
    
    * feat: DropdownContent 컴포넌트에 className props 추가
    
    * feat: useOutsideClick 훅 추가 및 Dropdown 컴포넌트에 적용
    
    * refactor: 키보드 접근성 고려하여 Dropdown 컴포넌트, useClickOutside 훅 수정
    
    * design: hover, focus 확인을 위한 기본 스타일 적용
    
    * design: DropdownItem이 DropdownContent 영역 밖으로 넘치지 않도록 수정
    
    * chore: 키보드로 Dropdown 조작 시 Enter 키로만 선택 가능하게 수정
    
    * chore: DropdownItem 컴포넌트의 props 이름 변경

---
+++ src/components/Dropdown/DropdownContent.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { PropsWithChildren } from 'react';
+import { cn } from '@/lib/utils';
+import { useDropdownContext } from './DropdownContext';
+
+interface DropdownContentProps {
+  className?: string;
+}
+
+const DropdownContent = ({
+  className,
+  children,
+}: PropsWithChildren<DropdownContentProps>) => {
+  const { isOpen } = useDropdownContext();
+
+  if (!isOpen) return null;
+
+  // TODO: 디자인 시안 나오면 스타일 수정 필요
+  const dropdownContentClasses = cn(
+    // default style
+    'absolute z-10 mt-2 w-48 overflow-hidden rounded-md border bg-white shadow-md',
+
+    // focus style
+    'focus:outline-none',
+
+    // custom style
+    className
+  );
+
+  return (
+    <ul
+      role='menu'
+      className={dropdownContentClasses}
+    >
+      {children}
+    </ul>
+  );
+};
+
+export default DropdownContent;
---
+++ src/components/Dropdown/DropdownItem.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { PropsWithChildren, useEffect, useRef } from 'react';
+import { cn } from '@/lib/utils';
+import { useDropdownContext } from './DropdownContext';
+
+interface DropdownItemProps {
+  label: string;
+  className?: string;
+}
+
+const DropdownItem = ({
+  label,
+  className,
+  children,
+}: PropsWithChildren<DropdownItemProps>) => {
+  const { isOpen, selectedItem, setSelectedItem, closeDropdown } =
+    useDropdownContext();
+  const itemRef = useRef<HTMLLIElement>(null);
+
+  const handleClick = () => {
+    setSelectedItem(label);
+    closeDropdown();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      setSelectedItem(label);
+      closeDropdown();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen && selectedItem === label) {
+      itemRef.current?.focus();
+    }
+  }, [isOpen, selectedItem, label]);
+
+  // TODO: 디자인 시안 나오면 스타일 수정 필요
+  const dropdownItemClasses = cn(
+    // default style
+    'cursor-pointer bg-white px-4 py-2  select-none',
+
+    // hover style
+    'hover:bg-gray-200',
+
+    // focus style
+    'focus:bg-gray-300 focus:outline-none',
+
+    // custom style
+    className
+  );
+
+  return (
+    <li
+      role='menuitem'
+      ref={itemRef}
+      tabIndex={0}
+      onClick={handleClick}
+      onKeyDown={handleKeyDown}
+      className={dropdownItemClasses}
+    >
+      {children}
+    </li>
+  );
+};
+
+export default DropdownItem;
