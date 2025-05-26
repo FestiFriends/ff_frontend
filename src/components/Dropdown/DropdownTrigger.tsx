1549e88a624779d17c36927ab02aae09fe40daa8
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2025-05-26 09:45:37 +0900

    [Feature/issue-051] Dropdown 컴포넌트 구현 (#81)
    * feat: Dropdown 컴포넌트 구현
    
    * feat: DropdownContent 컴포넌트에 className props 추가
    
    * feat: useOutsideClick 훅 추가 및 Dropdown 컴포넌트에 적용
    
    * refactor: 키보드 접근성 고려하여 Dropdown 컴포넌트, useClickOutside 훅 수정
    
    * design: hover, focus 확인을 위한 기본 스타일 적용
    
    * design: DropdownItem이 DropdownContent 영역 밖으로 넘치지 않도록 수정
    
    * chore: 키보드로 Dropdown 조작 시 Enter 키로만 선택 가능하게 수정
    
    * chore: DropdownItem 컴포넌트의 props 이름 변경

---
+++ src/components/Dropdown/DropdownTrigger.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { PropsWithChildren } from 'react';
+import { cn } from '@/lib/utils';
+import { useDropdownContext } from './DropdownContext';
+
+interface DropdownTriggerProps {
+  placeholder?: string;
+  className?: string;
+}
+
+const DropdownTrigger = ({
+  placeholder = '',
+  className,
+}: PropsWithChildren<DropdownTriggerProps>) => {
+  const { toggleDropdown, selectedItem } = useDropdownContext();
+
+  // TODO: 디자인 시안 나오면 스타일 수정 필요
+  const dropdownTriggerClasses = cn(
+    // default style
+    'inline-flex cursor-pointer items-center justify-between rounded-md border px-4 py-2',
+
+    // hover style
+    'hover:bg-gray-200',
+
+    // focus style
+    'focus:bg-gray-300 focus:outline-none',
+
+    // custom style
+    className
+  );
+
+  return (
+    <button
+      onClick={toggleDropdown}
+      className={dropdownTriggerClasses}
+    >
+      {selectedItem || placeholder}
+    </button>
+  );
+};
+
+export default DropdownTrigger;
---
+++ src/components/Dropdown/index.ts
@@ -0,0 +1,4 @@
+export { default as Dropdown } from './Dropdown';
+export { default as DropdownTrigger } from './DropdownTrigger';
+export { default as DropdownContent } from './DropdownContent';
+export { default as DropdownItem } from './DropdownItem';
