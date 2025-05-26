1d5f4b2a7b734db36352133cf9b6862e4c958e1e
SeongIn Ko <jjjk0605@naver.com>
2025-05-26 14:26:41 +0900

    feat: Modal 기본 open여부와 close시 트리거 이벤트 추가 구현

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,15 +3,27 @@
 import { PropsWithChildren, useState } from 'react';
 import { ModalContext } from './ModalContext';
 
-const Modal = ({ children }: PropsWithChildren) => {
-  const [open, setOpen] = useState(false);
+interface ModalProps {
+  defaultOpen?: boolean;
+  onClose?: () => void;
+}
+
+const Modal = ({
+  defaultOpen = false,
+  onClose,
+  children,
+}: PropsWithChildren<ModalProps>) => {
+  const [open, setOpen] = useState(defaultOpen);
   const openModal = () => setOpen(true);
-  const closeModal = () => setOpen(false);
+  const closeModal = () => {
+    setOpen(false);
+  };
 
   const ModalValue = {
     open,
     openModal,
     closeModal,
+    onClose,
   };
 
   return <ModalContext value={ModalValue}>{children}</ModalContext>;
