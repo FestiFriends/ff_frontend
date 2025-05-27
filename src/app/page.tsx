66bd690bb97e32317557eba026dd8798c376aa37
CHAN-H0 <lch0309k@gmail.com>
2025-05-28 02:36:11 +0900

    chore: page.tsx 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-import ProfileCard from '@/components/ProfileCard/ProfileCard';
-
-const Home = () => {
-  return (
-    <main className='min-h-screen bg-gray-50 py-10'>
-      <div className='mx-auto flex w-full max-w-4xl flex-col items-center gap-8 px-4'>
-        <ProfileCard userId='me' />
-        <ProfileCard userId='guest1' />
-        <ProfileCard userId='guest2' />
-        <ProfileCard userId='loading' />
-        <ProfileCard userId='1234' />
-      </div>
-    </main>
-  );
-};
+const Home = () => <div>hello</div>;
 
 export default Home;
