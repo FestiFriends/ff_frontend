f3df65a2df8d410f8e39ce9aa237f5d8458ee305
CHAN-H0 <lch0309k@gmail.com>
2025-05-27 18:29:26 +0900

    feat: 프로필 수정 클릭 시 이동처리 및 목데이터 유저 추가

--- src/mocks/handlers/profileHandlers.ts
+++ src/mocks/handlers/profileHandlers.ts
@@ -4,19 +4,63 @@ export const profileHandlers = [
   http.get('/api/profile/:userId', ({ params }) => {
     const { userId } = params;
 
+    if (userId === 'me') {
+      return HttpResponse.json({
+        profileImageUrl: '',
+        nickname: '락서니',
+        rating: 4.9,
+        gender: 'MALE',
+        description:
+          '공연과 여행을 사랑하는 자유로운 영혼, 락서니입니다 🎸🌍공연과 여행을 사랑하는 자유로운 영혼, 락서니입니다 🎸🌍공연과 여행을 사랑하는 자유로운 영혼, 락서니입니다 🎸🌍공연과 여행을 사랑하는 자유로운 영혼, 락서니입니다 🎸🌍',
+        sns: 'https://instagram.com/roxani.rocks',
+        tags: [
+          'INCHEON PENTAPORT MUSIC FESTIVAL',
+          '배려심 있어요',
+          '대화를 잘 이끌어요',
+        ],
+        isMyProfile: true,
+      });
+    }
+
+    if (userId === 'guest1') {
+      return HttpResponse.json({
+        profileImageUrl: '',
+        nickname: '비트보이',
+        rating: 4.2,
+        gender: 'MALE',
+        description: '락보다 힙합을 사랑하는 서울남자 🎧',
+        sns: 'https://instagram.com/beatboy',
+        tags: ['서울 재즈 페스티벌', '시간 잘 지켜요', '조용한 편이에요'],
+        isMyProfile: false,
+      });
+    }
+
+    if (userId === 'guest2') {
+      return HttpResponse.json({
+        profileImageUrl: '',
+        nickname: '페스타걸',
+        rating: 4.7,
+        gender: 'FEMALE',
+        description: '매년 페스티벌 전국투어 중인 진짜 마니아 🤘',
+        sns: 'https://instagram.com/festagirl',
+        tags: [
+          '부산 록 페스티벌',
+          '친절하고 매너가 좋아요',
+          '다음에도 함께하고 싶어요',
+        ],
+        isMyProfile: false,
+      });
+    }
+
     return HttpResponse.json({
       profileImageUrl: '',
-      nickname: '락서니',
-      rating: 4.9,
-      gender: 'MALE',
-      description: '공연과 여행을 사랑하는 자유로운 영혼, 락서니입니다 🎸🌍',
-      sns: 'https://instagram.com/roxani.rocks',
-      tags: [
-        'INCHEON PENTAPORT MUSIC FESTIVAL',
-        '배려심 있어요',
-        '대화를 잘 이끌어요',
-      ],
-      isMyProfile: userId === 'me',
+      nickname: `유저 ${userId}`,
+      rating: 0,
+      gender: 'ALL',
+      description: '알 수 없는 유저입니다.',
+      sns: '',
+      tags: [],
+      isMyProfile: false,
     });
   }),
 ];
