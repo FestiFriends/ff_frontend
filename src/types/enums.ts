9a84de4ce8609366a23045575f9d3c5610ca8693
songjihyun <songdaramji@gmail.com>
2025-05-28 14:49:38 +0900

    refactor: enum을 as const로 수정

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,11 +1,18 @@
-export enum GroupCategory {
-  COMPANION = 'COMPANION',
-  RIDE_SHARE = 'RIDE_SHARE',
-  ROOM_SHARE = 'ROOM_SHARE',
-}
+// constants/group.ts
 
-export enum Gender {
-  FEMALE = 'FEMALE',
-  MALE = 'MALE',
-  ALL = 'ALL',
-}
+export const GroupCategory = {
+  COMPANION: 'COMPANION',
+  RIDE_SHARE: 'RIDE_SHARE',
+  ROOM_SHARE: 'ROOM_SHARE',
+} as const;
+
+export type GroupCategoryType =
+  (typeof GroupCategory)[keyof typeof GroupCategory];
+
+export const Gender = {
+  FEMALE: 'FEMALE',
+  MALE: 'MALE',
+  ALL: 'ALL',
+} as const;
+
+export type GenderType = (typeof Gender)[keyof typeof Gender];
