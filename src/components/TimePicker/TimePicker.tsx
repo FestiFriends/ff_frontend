102df1c76686e0c2484930c090eb2faa8328964a
jadewisemann <jadewisemann@gmail.com>
2025-05-28 13:15:09 +0900

    feat: 시간, 분 간격  & 스타일용 prop 추가

--- src/components/TimePicker/TimePicker.tsx
+++ src/components/TimePicker/TimePicker.tsx
@@ -11,11 +11,35 @@ import {
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { cn } from '@/lib/utils';
 
-const TimePicker = ({ onChange }: { onChange?: (date: Date) => void }) => {
+interface TimePickerProps {
+  onChange?: (date: Date) => void;
+  size?: 'md' | 'sm' | 'lg';
+  disabled?: boolean;
+  hourStep?: number;
+  minuteStep?: number;
+  status?: 'error' | 'warn';
+}
+
+const TimePicker = ({
+  onChange,
+  size = 'md',
+  disabled = false,
+  hourStep = 1,
+  minuteStep = 1,
+  status,
+}: TimePickerProps) => {
   const [date, setDate] = React.useState<Date>(new Date());
   const [isOpen, setIsOpen] = React.useState(false);
 
-  const hours = Array.from({ length: 12 }, (_, i) => i + 1);
+  const hours = Array.from({ length: Math.ceil(12 / hourStep) }, (_, i) => {
+    const hour = i * hourStep + 1;
+    return hour > 12 ? hour - 12 : hour;
+  }).filter((hour) => hour <= 12);
+
+  const minutes = Array.from(
+    { length: Math.ceil(60 / minuteStep) },
+    (_, i) => i * minuteStep
+  ).filter((minute) => minute < 60);
 
   const handleTimeChange = (
     type: 'hour' | 'minute' | 'ampm',
@@ -34,11 +58,22 @@ const TimePicker = ({ onChange }: { onChange?: (date: Date) => void }) => {
       newDate.setHours(value === 'PM' ? hour12 + 12 : hour12);
     }
     setDate(newDate);
-    console.log('onChange', onChange);
-    console.log('typeof onChange', typeof onChange);
     if (typeof onChange === 'function') {
       onChange(newDate);
     }
+  };
+
+  const buttonClass = 'w-full justify-start text-left font-normal';
+
+  const buttonSizeClass = {
+    sm: 'text-xs',
+    md: 'text-sm',
+    lg: 'text-base',
+  };
+
+  const buttonStatusClass = {
+    error: 'border-red-600',
+    warn: 'border-orange-300',
   };
 
   return (
@@ -48,9 +83,12 @@ const TimePicker = ({ onChange }: { onChange?: (date: Date) => void }) => {
     >
       <PopoverTrigger asChild>
         <Button
+          disabled={disabled}
           variant='outline'
           className={cn(
-            'w-full justify-start text-left font-normal',
+            buttonClass,
+            buttonSizeClass[size],
+            status && buttonStatusClass[status],
             !date && 'text-muted-foreground'
           )}
         >
@@ -60,6 +98,7 @@ const TimePicker = ({ onChange }: { onChange?: (date: Date) => void }) => {
 
       <PopoverContent className='w-auto p-0'>
         <div className='flex flex-col divide-y sm:h-[300px] sm:flex-row sm:divide-x sm:divide-y-0'>
+          {/* 시간 */}
           <ScrollArea className='w-64 sm:w-auto'>
             <div className='flex p-2 sm:flex-col'>
               {hours.map((hour) => (
@@ -84,9 +123,10 @@ const TimePicker = ({ onChange }: { onChange?: (date: Date) => void }) => {
             />
           </ScrollArea>
 
+          {/* 분 */}
           <ScrollArea className='w-64 sm:w-auto'>
             <div className='flex p-2 sm:flex-col'>
-              {Array.from({ length: 12 }, (_, i) => i * 5).map((minute) => (
+              {minutes.map((minute) => (
                 <Button
                   key={minute}
                   size='icon'
@@ -106,6 +146,7 @@ const TimePicker = ({ onChange }: { onChange?: (date: Date) => void }) => {
             />
           </ScrollArea>
 
+          {/* am pm */}
           <ScrollArea className=''>
             <div className='flex p-2 sm:flex-col'>
               {['AM', 'PM'].map((ampm) => (
