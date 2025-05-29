f874b62f105ad76a11068467825a9565055fd158
Seokyung <xuejd17@gmail.com>
2025-05-29 15:49:08 +0900

    feat: utils 파일에 sortRangeValues 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,12 @@ export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
 // 욕설 포함된 text인지 확인(boolean값으로 return됨)
 export const hasProfanity = (text: string) => filter.isProfane(text);
+
+/**
+ * 두 숫자로 이루어진 range 값을 항상 [min, max] 형태로 정렬하는 함수
+ * @param value
+ * @returns [number, number]
+ */
+export const sortRangeValues = (value: [number, number]): [number, number] => {
+  return [Math.min(value[0], value[1]), Math.max(value[0], value[1])];
+};
