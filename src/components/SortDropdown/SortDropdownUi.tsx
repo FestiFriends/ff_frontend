1dcc520eae01dd1d565ee1e1e1ea74675b43cea9
CHAN-H0 <lch0309k@gmail.com>
2025-05-30 10:37:32 +0900

    feat: 정렬 드롭다운 UI 컴포넌트 SortDropdownUi 구현

---
+++ src/components/SortDropdown/SortDropdownUi.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownContent,
+  DropdownItem,
+} from '@/components/Dropdown';
+
+interface SortOption {
+  label: string;
+  value: string;
+}
+
+interface SortDropdownUiProps {
+  options: SortOption[];
+  selectedValue?: string;
+  onChange?: (value: string) => void;
+  placeholder?: string;
+}
+
+const SortDropdownUi = ({
+  options,
+  selectedValue,
+  onChange,
+  placeholder = '정렬',
+}: SortDropdownUiProps) => {
+  const selectedLabel = options.find((o) => o.value === selectedValue)?.label;
+  return (
+    <Dropdown>
+      <DropdownTrigger
+        value={selectedLabel}
+        placeholder={placeholder}
+      />
+      <DropdownContent>
+        {options.map((option) => (
+          <DropdownItem
+            key={option.value}
+            label={option.label}
+            value={option.value}
+            onClick={() => onChange?.(option.value)}
+          >
+            {option.label}
+          </DropdownItem>
+        ))}
+      </DropdownContent>
+    </Dropdown>
+  );
+};
+
+export default SortDropdownUi;
