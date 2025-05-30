d9d87d6bea0b304fc9776f0859dc762f45c9d24c
SeongIn Ko <jjjk0605@naver.com>
2025-05-30 16:05:19 +0900

    feat: authApi객체 선언 및 login, token refresh, logout 관련 함수 구현

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,20 +1,29 @@
-import { handleApiError } from '@/lib/api';
-import { KakaoLoginResponse } from '@/types/auth';
-import axios from 'axios';
+import apiFetcher from '@/lib/apiFetcher';
+import axiosFetcher from '@/lib/axiosFetcher';
+import { callLogout, callTokenUpdater } from '@/providers/AuthStoreProvider';
+import { ApiResponse } from '@/types/api';
+import { KakaoLoginResponse, TokenRefreshResponse } from '@/types/auth';
 
 export const authApi = {
   loginWithKakaoCode: async (code: string) => {
-    try {
-      const response = await axios.get<KakaoLoginResponse>(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/callback/kakao`,
-        {
-          params: { code },
-          withCredentials: true,
-        }
-      );
-      return response.data;
-    } catch (error) {
-      throw handleApiError(error);
+    return await axiosFetcher.get<KakaoLoginResponse>(
+      '/api/v1/auth/callback/kakao',
+      { params: { code } }
+    );
+  },
+
+  refreshToken: async () => {
+    const res =
+      await axiosFetcher.post<TokenRefreshResponse>('/api/v1/auth/token');
+
+    if (res.data.data) {
+      callTokenUpdater(res.data.data.accessToken);
     }
+    return res;
+  },
+
+  logout: async () => {
+    const res = await apiFetcher.post<ApiResponse>('/api/v1/auth/logout');
+    return res;
   },
 };
