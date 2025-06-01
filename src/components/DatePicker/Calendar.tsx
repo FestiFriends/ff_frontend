882daa743ff1cdf89acf0ff866d7fe62657f9e90
Seokyung <xuejd17@gmail.com>
2025-06-01 16:46:20 +0900

    feat: DatePicker에서 사용할 Calendar 컴포넌트 구현

---
+++ src/components/DatePicker/Calendar.tsx
@@ -0,0 +1,95 @@
+import { useMemo } from 'react';
+import { cn } from '@/lib/utils';
+import {
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  isAfter,
+  isBefore,
+  isSameDay,
+  startOfMonth,
+} from 'date-fns';
+
+interface CalendarProps {
+  month?: Date;
+  startDate?: Date | null;
+  endDate?: Date | null;
+  onDateClick?: (date: Date) => void;
+  className?: string;
+}
+
+const WEEKDAYS_KR = ['일', '월', '화', '수', '목', '금', '토'];
+
+const Calendar = ({
+  month = new Date(),
+  startDate,
+  endDate,
+  onDateClick,
+  className,
+}: CalendarProps) => {
+  const days = useMemo(() => {
+    const start = startOfMonth(month);
+    const end = endOfMonth(month);
+    const daysInMonth = eachDayOfInterval({ start, end });
+
+    const startWeekDay = start.getDay();
+    const emptyDays = Array.from({ length: startWeekDay }, () => null);
+
+    return [...emptyDays, ...daysInMonth];
+  }, [month]);
+
+  const renderDay = (day: Date | null, index: number) => {
+    if (day === null) return <div key={`empty-${index}`}></div>;
+
+    const isStart = startDate && isSameDay(day, startDate);
+    const isEnd = endDate && isSameDay(day, endDate);
+    const isRange =
+      startDate && endDate && isAfter(day, startDate) && isBefore(day, endDate);
+
+    // TODO: 디자인 시안 나오면 스타일 수정 필요
+    const dayClasses = cn(
+      // default style
+      'flex h-10 w-10 items-center justify-center rounded-full',
+
+      // clickable style
+      onDateClick && 'cursor-pointer',
+
+      // startDate style
+      isStart && 'bg-blue-500 text-white',
+
+      // endDate style
+      isEnd && 'bg-blue-500 text-white',
+
+      // rangeDate style
+      isRange && 'bg-blue-100 text-blue-700'
+    );
+
+    return (
+      <button
+        key={day.toISOString()}
+        onClick={() => onDateClick?.(day)}
+        className={dayClasses}
+      >
+        <span>{format(day, 'd')}</span>
+      </button>
+    );
+  };
+
+  const calendarClasses = cn('w-full', className);
+
+  return (
+    <div className={calendarClasses}>
+      <div className='text-center'></div>
+      <div className='grid grid-cols-7 gap-1 text-center'>
+        {WEEKDAYS_KR.map((d) => (
+          <div key={d}>{d}</div>
+        ))}
+      </div>
+      <div className='grid grid-cols-7 gap-1 text-center'>
+        {days.map((day, index) => renderDay(day, index))}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
