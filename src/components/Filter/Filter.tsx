156ceb66c9b7183d86c1cdd6282fae7cb440a376
CHAN-H0 <130299425+CHAN-H0@users.noreply.github.com>
2025-06-02 11:06:04 +0900

    [Feature/issue-071] Filter 컴포넌트 기능 구현 및 테스트, 문서 (#147)
    * feat: Filter 컴포넌트 기본 틀 및 props 정의
    
    * feat: DropdownItems onClick 프롭스 추가, Filter 컴포넌트에Dropdown 컴포넌트 적용
    
    * feat: Filter 컴포넌트에 DropdownTrigger placeholder 기반 버튼 구조 적용
    
    * chore: FilterUi로 이름 변경
    
    * feat: useMultiLevelFilter 훅 스켈레톤 작성 1단계 상태관리
    
    * feat: 다단계 필터를 위한 useMultiLevelFilter 훅 추가
    
    * feat: 다단계 필터링을 지원하게 useMultiLevelFilter 훅 확장
    
    * fix: DropdownTrigger에서 selectedItem 대신 props.value 사용하도록 수정
    
    * feat: 다단계 필터를 위한 Filter 컴포넌트 구현
    
    * refactor: onClick 핸들러 분리해서 랜더 최적화
    
    * test: 리펙터링된 Dropdown 컴포넌트 구조에 맞게 테스트  코드 수정
    
    * test: Filter 컴포넌트 테스트
    
    * docs: Filter 컴포넌트 README 작성
    
    * docs: README 파일구조 추가
    
    * fix: DropdownTrigger에 선택된 label이 표시되도록 수정
    
    * refactor: DropdownItem에 value props 추가 및 선택 기준 value로 통일
    
    * refactor: DropdownItem에서 불필요한 label props 제거

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,54 @@
+import {
+  useMultiLevelFilter,
+  MultiLevelData,
+} from '@/hooks/useMultiLevelFilter/useMultiLevelFilter';
+import FilterUi from '@/components/Filter/FilterUi';
+import { useCallback } from 'react';
+
+interface FilterProps {
+  data: MultiLevelData[];
+  levelPlaceholders?: string[];
+  onChange?: (values: string[]) => void;
+}
+
+const Filter = ({ data, levelPlaceholders, onChange }: FilterProps) => {
+  const { selectedValues, optionsByLevel, setValueAtLevel } =
+    useMultiLevelFilter(data);
+
+  const levelHandleChange = useCallback(
+    (level: number) => (val: string) => {
+      setValueAtLevel(level, val);
+
+      const updated = [...selectedValues.slice(0, level), val];
+      onChange?.(updated);
+    },
+    [onChange, setValueAtLevel]
+  );
+
+  return (
+    <div className='space-y-4'>
+      {optionsByLevel.map((options, i) => {
+        const value = selectedValues[i];
+        const hasParentSelected =
+          i === 0 || selectedValues[i - 1] !== undefined;
+
+        if (!hasParentSelected || options.length === 0) return null;
+
+        return (
+          <FilterUi
+            key={i}
+            placeholder={levelPlaceholders?.[i] ?? `단계 ${i + 1} 선택`}
+            value={value}
+            options={options.map((opt) => ({
+              label: opt.label,
+              value: opt.value,
+            }))}
+            onChange={levelHandleChange(i)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Filter;
