4cd376a6aa2596161d4cef605fa91463d8fd5918
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2025-06-02 11:06:46 +0900

    [Feature/issue-070] MultiSlider 구현, 테스트 코드 작성 및 문서화 (#150)
    * [Feature/issue-114] useImageUploader hook 함수 오버로딩으로 타입 분기 (#116)
    
    * refactor: useImageUploader hook 함수 오버로딩을 통해 반환타입 분기
    
    * test: single모드일 경우 remove에 인자 안넘기도록 변경
    
    * docs: useImageUploader 변경 사항에 맞춰 문서 수정
    
    * [Feature/issue-036] Poster 컴포넌트 구현 (#42)
    
    * chore: 브랜치명 오타 수정 임시저장
    
    * feat: Poster 기본 props 설정
    
    * feat: Poster 컴포넌트 사이즈 프리셋 설정
    
    * chore: Poster 컴포넌트 사이즈 프리셋 lg 높이 수정
    
    * chore: page.tsx 파일 복구
    
    * chore: Poster 컴포넌트 width height를 fill로 변경
    
    * chore: Poster 컴포넌트 clsx대신 cn으로 변경
    
    * chore: Poster 컴포넌트 div로 감싸기
    
    * chore: Poster 컴포넌트 div 태그에 className 적용
    
    * feat: Poster 컴포넌트 배경색상 추가
    
    * chore: 임포트 따옴표 변경
    
    * test: Poster 컴포넌트 테스트
    
    * docs: Poster 컴포넌트 README 작성 (#120)
    
    * [Fix/issue-111] Button Group 테스트 메시지 수정 (#134)
    
    * refactor: error 메시지 분리
    
    * test: 에러 메시지 분리에 대응
    
    * feat: MultiSlider 컴포넌트 기본 기능 구현
    
    * chore: slider-thumbs css firefox 지원 추가
    
    * fix: 커스텀 thumb과 input range의 thumb 위치 같게 보정
    
    * design: valuePosition 스타일 추가
    
    * design: valuePosition, 커스텀 thumb에 그림자 추가
    
    * fix: range의 left, right thumb 값이 서로를 초과하지 않도록 수정
    
    * feat: range에 marks 옵션 추가
    
    * feat: utils 파일에 sortRangeValues 함수 추가
    
    * refactor: sortRangeValues 함수 사용하여 range값 보정
    
    * test: MultiSlider 테스트 작성
    
    * docs: MultiSlider 리드미 문서 작성
    
    * design: 컴포넌트 wrapper 너비 수정
    
    * design: range thumb 커서 스타일 제거
    
    ---------
    
    Co-authored-by: SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
    Co-authored-by: CHAN-H0 <130299425+CHAN-H0@users.noreply.github.com>
    Co-authored-by: CHAN-H0 <lch0309k@gmail.com>
    Co-authored-by: Jade Wisemann <139527412+jadewisemann@users.noreply.github.com>

---
+++ src/components/MultiSlider/MultiSlider.tsx
@@ -0,0 +1,196 @@
+'use client';
+
+import { cn, sortRangeValues } from '@/lib/utils';
+import { useState } from 'react';
+
+interface MultiSliderProps {
+  min?: number;
+  max?: number;
+  step?: number;
+  disabled?: boolean;
+  defaultValue?: [number, number];
+  value?: [number, number];
+  onChange?: (value: [number, number]) => void;
+  valuePosition?: 'top' | 'bottom' | 'none';
+  marks?: Record<number, string>;
+}
+
+const thumbSize = 20;
+
+// TODO: 디자인 시안 나오면 스타일 수정 필요
+const inputRangeClasses = `pointer-events-none absolute z-2 h-3 w-full appearance-none bg-transparent opacity-0 [&::-moz-range-thumb]:pointer-events-auto [&::-webkit-slider-thumb]:pointer-events-auto [&::-moz-range-thumb]:h-[20px] [&::-webkit-slider-thumb]:h-[20px] [&::-moz-range-thumb]:w-[20px] [&::-webkit-slider-thumb]:w-[20px] [&::-moz-range-thumb]:appearance-none [&::-webkit-slider-thumb]:appearance-none [&::-moz-range-thumb]:rounded-none [&::-webkit-slider-thumb]:rounded-none [&::-moz-range-thumb]:border-0 [&::-webkit-slider-thumb]:border-0 [&::-moz-range-thumb]:bg-red-400 [&::-webkit-slider-thumb]:bg-red-400`;
+
+const MultiSlider = ({
+  min = 0,
+  max = 100,
+  step,
+  disabled,
+  defaultValue = [min, max],
+  value,
+  onChange,
+  marks,
+  valuePosition = 'top',
+}: MultiSliderProps) => {
+  const isControlled = value !== undefined;
+  const [internalValue, setInternalValue] = useState<[number, number]>(
+    sortRangeValues(defaultValue)
+  );
+  const currentValue = isControlled ? sortRangeValues(value) : internalValue;
+
+  const handleChange = (
+    index: 0 | 1,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = Number(e.target.value);
+
+    const newValue: [number, number] =
+      index === 0 ? [value, currentValue[1]] : [currentValue[0], value];
+
+    const sorted = sortRangeValues(newValue);
+
+    if (!isControlled) setInternalValue(sorted);
+    onChange?.(sorted);
+  };
+
+  const getPercent = (value: number) => ((value - min) / (max - min)) * 100;
+
+  const getThumbOffset = (index: 0 | 1) =>
+    thumbSize / 2 - ((thumbSize / 2) * getPercent(currentValue[index])) / 50;
+
+  // TODO: 디자인 시안 나오면 스타일 수정 필요
+  const rangeClasses = cn(
+    // default style
+    'absolute top-0 right-0 bottom-0 left-0 z-2 rounded-full bg-blue-400',
+
+    // disabled style
+    disabled && 'bg-gray-400'
+  );
+
+  const thumbClasses = cn(
+    // default style
+    'absolute top-1/2 z-3 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-blue-400 bg-white',
+
+    // disabled style
+    disabled && 'border-gray-400'
+  );
+
+  const thumbValueClasses = cn(
+    // default style
+    'absolute left-1/2 -translate-x-1/2 rounded-sm bg-white px-1 py-0.5 text-xs text-gray-600 shadow-sm',
+
+    // valuePosition style
+    {
+      '-top-full -translate-y-1/2': valuePosition === 'top',
+      '-bottom-full translate-y-1/2': valuePosition === 'bottom',
+      hidden: valuePosition === 'none',
+    }
+  );
+
+  // TODO: 디자인 시안 나오면 스타일 수정 필요
+  return (
+    <div className='relative w-full'>
+      <input
+        type='range'
+        id='range-left'
+        disabled={disabled}
+        min={min}
+        max={max}
+        step={step}
+        value={currentValue[0]}
+        onChange={(e) => handleChange(0, e)}
+        className={inputRangeClasses}
+      />
+      <input
+        type='range'
+        id='range-right'
+        disabled={disabled}
+        min={min}
+        max={max}
+        step={step}
+        value={currentValue[1]}
+        onChange={(e) => handleChange(1, e)}
+        className={inputRangeClasses}
+      />
+
+      <div
+        id='slider'
+        className='relative z-1 h-3'
+      >
+        <span
+          id='track'
+          className='absolute top-0 right-0 bottom-0 left-0 z-1 rounded-full bg-gray-300'
+        ></span>
+        <span
+          id='range'
+          className={rangeClasses}
+          style={{
+            left: `${getPercent(Math.min(currentValue[0], currentValue[1]))}%`,
+            right: `${100 - getPercent(Math.max(currentValue[0], currentValue[1]))}%`,
+          }}
+        ></span>
+        <span
+          id='thumb-left'
+          className={thumbClasses}
+          style={{
+            width: `${thumbSize}px`,
+            height: `${thumbSize}px`,
+            left: `calc(${getPercent(currentValue[0])}% + ${getThumbOffset(0)}px)`,
+            boxShadow: `0 4px 6px -1px var(--tw-shadow-color, rgb(0 0 0 / 0.1)), 0 2px 4px -2px`,
+          }}
+        >
+          {valuePosition !== 'none' && (
+            <span
+              aria-label='thumb-value'
+              className={thumbValueClasses}
+            >
+              {currentValue[0]}
+            </span>
+          )}
+        </span>
+        <span
+          id='thumb-right'
+          className={thumbClasses}
+          style={{
+            width: `${thumbSize}px`,
+            height: `${thumbSize}px`,
+            left: `calc(${getPercent(currentValue[1])}% + ${getThumbOffset(1)}px)`,
+            boxShadow: `0 4px 6px -1px var(--tw-shadow-color, rgb(0 0 0 / 0.1)), 0 2px 4px -2px`,
+          }}
+        >
+          {valuePosition !== 'none' && (
+            <span
+              aria-label='thumb-value'
+              className={thumbValueClasses}
+            >
+              {currentValue[1]}
+            </span>
+          )}
+        </span>
+      </div>
+
+      {marks && (
+        <div className='relative'>
+          <div
+            className='absolute text-gray-600 select-none'
+            style={{ left: `${thumbSize / 2}px`, right: `${thumbSize / 2}px` }}
+          >
+            {Object.entries(marks).map(([key, label]) => {
+              return (
+                <span
+                  key={key}
+                  className='absolute inline-flex -translate-x-1/2 flex-col items-center text-center text-xs text-gray-600'
+                  style={{ left: `${getPercent(Number(key))}%` }}
+                >
+                  <span className='mb-0.5 h-0.75 w-px bg-gray-400' />
+                  <span className='whitespace-nowrap'>{label}</span>
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MultiSlider;
