04b6d397bff9886cf4d6f0f3b5dace494cca57c1
Seokyung <xuejd17@gmail.com>
2025-06-03 11:37:49 +0900

    rename: performanceDetailHandlers 파일명 변경

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,5 @@
 import { setupWorker } from 'msw/browser';
-import { performanceDetailHandlers } from './handlers/performanceDetail';
+import { performanceDetailHandlers } from './handlers/performanceDetailHandlers';
 import { profileHandlers } from './handlers/profileHandlers';
 
 export const worker = setupWorker(
