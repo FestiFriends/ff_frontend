ccd48696c8976f7d5e5024d81a38378cf8a9a702
Seokyung <xuejd17@gmail.com>
2025-06-04 18:48:02 +0900

    test: 누락된 공연 상세 조회 mock handler 추가

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,5 +1,10 @@
 import { setupServer } from 'msw/node';
+import { performanceDetailHandlers } from './handlers/performanceDetailHandlers';
 import { performancesHandlers } from './handlers/performancesHandlers';
 import { reviewsHandlers } from './handlers/reviewsHandlers';
 
-export const server = setupServer(...performancesHandlers, ...reviewsHandlers);
+export const server = setupServer(
+  ...performancesHandlers,
+  ...reviewsHandlers,
+  ...performanceDetailHandlers
+);
