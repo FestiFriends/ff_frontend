7bad536c59d570aaac337014fe9dce07cc876d87
SeongIn Ko <jjjk0605@naver.com>
2025-06-05 15:38:53 +0900

    feat: 알림 조회 무한 스크롤 hook 구현

---
+++ src/hooks/notificationHooks/notificationHooks.ts
@@ -0,0 +1,23 @@
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { PERFORMANCES_QUERY_KEYS } from '@/constants/queryKeys';
+import { notificationsApi } from '@/services/notificationsService';
+import { ApiResponse, CursorRequest } from '@/types/api';
+import { GetNotificationsResponse } from '@/types/notification';
+
+export const useInfiniteNotifications = (size: CursorRequest['size']) =>
+  useInfiniteQuery<
+    AxiosResponse<GetNotificationsResponse>,
+    ApiResponse,
+    InfiniteData<AxiosResponse<GetNotificationsResponse>>,
+    string[],
+    number | undefined
+  >({
+    queryKey: [PERFORMANCES_QUERY_KEYS.notifications],
+    queryFn: ({ pageParam }) =>
+      notificationsApi.getNotifications({ cursorId: pageParam, size }),
+
+    getNextPageParam: (lastPage) =>
+      lastPage.data?.hasNext ? lastPage.data?.cursorId : undefined,
+    initialPageParam: undefined,
+  });
