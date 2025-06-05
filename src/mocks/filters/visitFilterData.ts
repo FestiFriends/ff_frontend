f8758c1143b7d105cbdd924295bdd5ae3ad02d8c
CHAN-H0 <lch0309k@gmail.com>
2025-06-05 17:58:57 +0900

    feat: 내한 목데이터 및 필터 추가

---
+++ src/mocks/filters/visitFilterData.ts
@@ -0,0 +1,6 @@
+import { MultiLevelData } from '@/hooks/useMultiLevelFilter/useMultiLevelFilter';
+
+export const visitFilterData: MultiLevelData[] = [
+  { label: '내한', value: '내한' },
+  { label: '국내', value: '국내' },
+];
