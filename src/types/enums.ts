28a7a4ffa4ebadc0bc2482ef504b143a4703a469
SeongIn Ko <jjjk0605@naver.com>
2025-06-06 20:21:56 +0900

    feat: 리뷰 관련 타입 정의 및 enum수정

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,11 +1,10 @@
 export const GroupCategory = {
-  COMPANION: 'COMPANION',
-  RIDE_SHARE: 'RIDE_SHARE',
-  ROOM_SHARE: 'ROOM_SHARE',
+  COMPANION: '같이 동행',
+  RIDE_SHARE: '같이 탑승',
+  ROOM_SHARE: '같이 숙박',
 } as const;
 
-export type GroupCategoryType =
-  (typeof GroupCategory)[keyof typeof GroupCategory];
+export type GroupCategoryType = keyof typeof GroupCategory;
 
 export const Gender = {
   FEMALE: 'FEMALE',
@@ -27,13 +26,13 @@ export const ReportReason = {
 export type ReportReasonType = (typeof ReportReason)[keyof typeof ReportReason];
 
 export const ReviewTag = {
-  PUNCTUAL: '시간 약속을 잘 지켜요', // 시간 약속을 잘 지켜요
-  POLITE: '친절하고 매너가 좋아요', // 친절하고 매너가 좋아요
-  COMFORTABLE: '편안한 분위기였어요', // 편안한 분위기였어요
   COMMUNICATIVE: '대화가 잘 통했어요', // 대화가 잘 통했어요
-  CLEAN: '청결하고 깔끔했어요', // 청결하고 깔끔했어요
-  RESPONSIVE: '소통이 잘 되고 응답이 빨라요', // 소통이 잘 되고 응답이 빨라요
   RECOMMEND: '다음에도 함께하고 싶어요', // 다음에도 함께하고 싶어요
+  POLITE: '친절하고 매너가 좋아요', // 친절하고 매너가 좋아요
+  PUNCTUAL: '시간 약속을 잘 지켜요', // 시간 약속을 잘 지켜요
+  CLEAN: '청결하고 깔끔했어요', // 청결하고 깔끔했어요
+  COMFORTABLE: '편안한 분위기였어요', // 편안한 분위기였어요
+  RESPONSIVE: '소통이 잘 되고 응답이 빨라요', // 소통이 잘 되고 응답이 빨라요
 } as const;
 
 export type ReviewTagType = keyof typeof ReviewTag;
--- src/types/reviews.ts
+++ src/types/reviews.ts
@@ -1,7 +1,7 @@
-import { ApiResponse } from './api';
+import { ApiResponse, CursorResponse } from './api';
 import { GroupCategoryType, ReviewTagType } from './enums';
 
-export interface RecentReviewsData {
+export interface ReviewGroupInfo {
   groupId: string; // 모임 ID
   performance: {
     id: string; // 공연 ID
@@ -12,6 +12,9 @@ export interface RecentReviewsData {
   category: GroupCategoryType; // 모임 카테고리 종류
   groupStartDate: string; // 모임 시작 날짜 (ISO 8601)
   groupEndDate: string; // 모임 종료 날짜 (ISO 8601)
+}
+
+export interface RecentReviewsData extends ReviewGroupInfo {
   reviews: {
     reviewId: string; // 리뷰 ID
     rating: number; // 별점
@@ -22,3 +25,40 @@ export interface RecentReviewsData {
 }
 
 export type RecentReviewsResponse = ApiResponse<RecentReviewsData[]>;
+
+export interface PostReviewRequest {
+  groupId: string; // 모임 ID
+  targetUserId: string; // 리뷰 대상 사용자 ID
+  rating: number; // 별점 (1~5, 0.5단위)
+  content?: string; // 직접 작성한 리뷰 내용 (optional)
+  defaultTag: ReviewTagType[]; // ReviewTag (ENUM), 선택한 기본 문구 리스트 (optional)
+}
+
+export interface WritableReviewsData extends ReviewGroupInfo {
+  memberCount: number; // 그룹 인원 수
+  reviews: {
+    targetUserId: string; // 리뷰 대상 사용자 ID
+    targetUserProfileImage: string; // 리뷰 대상 사용자 프로필 이미지
+    targetUserName: string; // 리뷰 대상 사용자 이름
+  }[];
+}
+
+export type WritableReviewsResponse = ApiResponse<WritableReviewsData[]>
+  & CursorResponse;
+
+export interface WrittenReviewsData extends ReviewGroupInfo {
+  memberCount: number; // 그룹 인원 수
+  reviews: {
+    reviewId: string; // 리뷰 ID
+    targetUserId: string; // 리뷰 대상 사용자 ID
+    targetUserName: string; // 리뷰 대상 사용자 이름
+    targetUserProfileImage: string; // 리뷰 대상 사용자 프로필 이미지
+    rating: number; // 별점
+    content?: string; // 리뷰 추가 입력 내용
+    defaultTag: ReviewTagType[]; // ReviewTag (ENUM)
+    createdAt: string; // 리뷰 생성 날짜 (ISO 8601)
+  }[];
+}
+
+export type WrittenReviewsResponse = ApiResponse<WrittenReviewsData[]>
+  & CursorResponse;
