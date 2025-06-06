5a239fdde458772afe68a98861eb5d9d179d0958
SeongIn Ko <jjjk0605@naver.com>
2025-06-06 20:33:01 +0900

    feat: 리뷰 작성 hook 구현 및 캐시를 사용한 낙관적 업데이트 구현

--- src/hooks/reviewHooks/reviewHooks.ts
+++ src/hooks/reviewHooks/reviewHooks.ts
@@ -1,9 +1,16 @@
-import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
+import {
+  InfiniteData,
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 import { REVIEWS_QUERY_KEYS } from '@/constants/queryKeys';
 import { reviewsApi } from '@/services/reviewsService';
 import { ApiResponse, CursorRequest } from '@/types/api';
 import {
+  PostReviewRequest,
+  WritableReviewsData,
   WritableReviewsResponse,
   WrittenReviewsResponse,
 } from '@/types/reviews';
@@ -41,3 +48,56 @@ export const useInfiniteWritableReviews = (size?: CursorRequest['size']) =>
       lastPage.data?.hasNext ? lastPage.data?.cursorId : undefined,
     initialPageParam: undefined,
   });
+
+export const usePostWriteReview = () => {
+  const queryClient = useQueryClient();
+  return useMutation<ApiResponse, ApiResponse, PostReviewRequest>({
+    mutationFn: reviewsApi.postWriteReview,
+    onSuccess: (_, variables) => {
+      const queryKey = [
+        REVIEWS_QUERY_KEYS.reviews,
+        REVIEWS_QUERY_KEYS.writable,
+      ];
+
+      queryClient.setQueryData(
+        queryKey,
+        (oldData: InfiniteData<AxiosResponse<WritableReviewsResponse>>) => {
+          if (!oldData) return oldData;
+
+          const newPages = oldData.pages.map((page) => {
+            const updatedGroups = page.data.data
+              ?.map((group: WritableReviewsData) => {
+                const filteredReviews = group.reviews.filter(
+                  (r) => r.targetUserId !== variables.targetUserId
+                );
+
+                if (filteredReviews.length === 0) return null;
+
+                return {
+                  ...group,
+                  reviews: filteredReviews,
+                };
+              })
+              .filter((group) => group !== null);
+
+            return {
+              ...page,
+              data: {
+                ...page.data,
+                data: updatedGroups,
+              },
+            };
+          });
+
+          return {
+            ...oldData,
+            pages: newPages,
+          };
+        }
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [REVIEWS_QUERY_KEYS.reviews] });
+    },
+  });
+};
