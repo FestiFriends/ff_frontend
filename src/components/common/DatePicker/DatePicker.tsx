08428bc8bb56d41b10eb71cbee42597bb1d8310a
Seokyung <xuejd17@gmail.com>
2025-06-07 21:03:18 +0900

    fix: DatePicker startDate와 endDate가 모두 있으면 null로 초기화되도록 수정

--- src/components/common/DatePicker/DatePicker.tsx
+++ src/components/common/DatePicker/DatePicker.tsx
@@ -37,8 +37,12 @@ const DatePicker = ({
     (date: Date) => {
       const { startDate, endDate } = selectedRange;
 
-      if (!startDate || (startDate && endDate)) {
+      if (!startDate) {
         const newRange = { startDate: date, endDate: null };
+        setSelectedRange(newRange);
+        onChange(newRange);
+      } else if (startDate && endDate) {
+        const newRange = { startDate: null, endDate: null };
         setSelectedRange(newRange);
         onChange(newRange);
       } else {
