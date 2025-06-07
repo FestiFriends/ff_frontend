0e015ec4f145e43cbf61c778647ce83342721974
Seokyung <xuejd17@gmail.com>
2025-06-07 18:25:17 +0900

    chore: 불필요한 queryKey 제거

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -2,7 +2,6 @@ export const PERFORMANCES_QUERY_KEYS = {
   performances: 'performances',
   topFavorites: 'topFavorites',
   topByGroupCount: 'topByGroupCount',
-  performanceDetail: 'performanceDetail',
 };
 
 export const NOTIFICATIONS_QUERY_KEYS = {
