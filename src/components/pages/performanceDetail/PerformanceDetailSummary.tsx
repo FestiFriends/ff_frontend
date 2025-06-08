5147de35ca5a90b4775feb740e2236165b027d7a
Seokyung <xuejd17@gmail.com>
2025-06-09 00:31:41 +0900

    design: PerformanceDetailSummary 컴포넌트 디자인 적용

--- src/components/pages/performanceDetail/PerformanceDetailSummary.tsx
+++ src/components/pages/performanceDetail/PerformanceDetailSummary.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useState } from 'react';
-import { format } from 'date-fns';
+import { useMemo, useState } from 'react';
 import Poster from '@/components/common/poster/Poster';
 import Toast from '@/components/common/Toast/Toast';
 import LikeIcon from '@/components/icons/LikeIcon';
@@ -15,12 +14,18 @@ interface PerformanceDetailSummaryProps {
   performanceDetail: Performance;
 }
 
+type InfoItem = {
+  label: string;
+  value: React.ReactNode;
+};
+
 const PerformanceDetailSummary = ({
   performanceDetail,
 }: PerformanceDetailSummaryProps) => {
   const isLoggedIn = useAuthStore((state) => state.isLoggedin);
   const [showToast, setShowToast] = useState(false);
   const { mutate } = usePatchPerformanceLiked();
+  const { location, place } = formatLocation(performanceDetail.location);
 
   const toggleLike = () => {
     if (!isLoggedIn) {
@@ -33,6 +38,70 @@ const PerformanceDetailSummary = ({
     });
   };
 
+  const performanceInfoList: InfoItem[] = useMemo(
+    () => [
+      {
+        label: '기간',
+        value: `${format(performanceDetail.startDate, 'yy.MM.dd')} ~ ${format(performanceDetail.endDate, 'yy.MM.dd')}`,
+      },
+      {
+        label: '지역',
+        value: location,
+      },
+      {
+        label: '공연 장소',
+        value: place,
+      },
+      {
+        label: '출연진',
+        value: (
+          <div className='flex flex-wrap items-center gap-x-1'>
+            {performanceDetail.cast.map((cast: string) => (
+              <span
+                className='underline decoration-solid [text-decoration-thickness:auto] [text-underline-offset:auto] [text-decoration-skip-ink:none] [text-underline-position:from-font]'
+                key={cast}
+              >
+                {cast}
+              </span>
+            ))}
+          </div>
+        ),
+      },
+      {
+        label: '제작진',
+        value: performanceDetail.crew?.join(', '),
+      },
+      { label: '공연 시간', value: performanceDetail.runtime },
+      { label: '기획사', value: performanceDetail.agency },
+      {
+        label: '티켓 가격',
+        value: performanceDetail.price.map((price: string) => (
+          <p key={price}>{price}</p>
+        )),
+      },
+      { label: '연령', value: performanceDetail.age },
+    ],
+    [performanceDetail, location, place]
+  );
+
+  const renderPerformanceInfoList = () => (
+    <div className='table border-separate [border-spacing:0_16px]'>
+      {performanceInfoList.map(({ label, value }) => (
+        <div
+          key={label}
+          className='table-row'
+        >
+          <span className='table-cell pr-3 align-top text-16_B leading-normal tracking-[-0.4px] whitespace-nowrap text-gray-700'>
+            {label}
+          </span>
+          <span className='table-cell align-top text-16_M leading-normal tracking-[-0.4px] text-gray-700'>
+            {value}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+
   return (
     <>
       {showToast && (
@@ -44,87 +113,41 @@ const PerformanceDetailSummary = ({
         />
       )}
 
-      <div>
-        <div className='flex items-center justify-center'>
-          <Poster
-            src={performanceDetail.poster || ''}
-            alt={performanceDetail.title}
-            className='h-100 max-h-[430px] w-full'
-          />
-        </div>
+      <div className='flex flex-col gap-5 px-4 pt-5 pb-7.5'>
+        {/* 공연 포스터 */}
+        <Poster
+          src={performanceDetail.poster || ''}
+          alt={performanceDetail.title}
+          className='aspect-[2/3] h-auto max-h-[60vh] w-full'
+        />
 
-        <div className='flex items-center justify-between'>
-          <h2 className='text-lg font-bold'>{performanceDetail.title}</h2>
-          <div>
-            <button onClick={toggleLike}>
-              <LikeIcon type={performanceDetail.isLiked ? 'active' : 'empty'} />
-            </button>
-            <button>
-              <ShareIcon />
-            </button>
-          </div>
-        </div>
-
-        <div>
-          <div className='grid grid-cols-2'>
-            <span>기간</span>
-            <span>
-              {format(performanceDetail.startDate, 'yy.MM.dd')} ~{' '}
-              {format(performanceDetail.endDate, 'yy.MM.dd')}
-            </span>
-          </div>
-
-          <div className='grid grid-cols-2'>
-            <span>지역</span>
-            <span>{formatLocation(performanceDetail.location).location}</span>
-          </div>
-
-          <div className='grid grid-cols-2'>
-            <span>공연장소</span>
-            <span>{formatLocation(performanceDetail.location).place}</span>
-          </div>
-
-          <div className='grid grid-cols-2'>
-            <span>출연진</span>
-            <div>
-              {performanceDetail.cast.map((cast: string) => (
-                <span key={cast}>{cast}</span>
-              ))}
+        <div className='flex flex-col gap-5'>
+          {/* 공연명, 찜, 공유 */}
+          <div className='flex items-center justify-between'>
+            <h2 className='text-18_B tracking-[-0.45px] text-gray-800'>
+              {performanceDetail.title}
+            </h2>
+            <div className='flex items-center gap-3'>
+              <button
+                onClick={toggleLike}
+                aria-label='찜 버튼'
+                className='flex h-7.5 w-7.5 items-center justify-center rounded-full bg-gray-25'
+              >
+                <LikeIcon
+                  type={performanceDetail.isLiked ? 'active' : 'empty'}
+                />
+              </button>
+              <button
+                aria-label='공유 버튼'
+                className='flex h-7.5 w-7.5 items-center justify-center rounded-full bg-gray-25'
+              >
+                <ShareIcon />
+              </button>
             </div>
           </div>
 
-          <div className='grid grid-cols-2'>
-            <span>제작진</span>
-            <div>
-              {performanceDetail.crew?.map((crew: string) => (
-                <span key={crew}>{crew}</span>
-              ))}
-            </div>
-          </div>
-
-          <div className='grid grid-cols-2'>
-            <span>공연 시간</span>
-            <span>{performanceDetail.runtime}</span>
-          </div>
-
-          <div className='grid grid-cols-2'>
-            <span>기획사</span>
-            <span>{performanceDetail.agency}</span>
-          </div>
-
-          <div className='grid grid-cols-2'>
-            <span>티켓 가격</span>
-            <div className='flex flex-col'>
-              {performanceDetail.price.map((price: string) => (
-                <span key={price}>{price}</span>
-              ))}
-            </div>
-          </div>
-
-          <div className='grid grid-cols-2'>
-            <span>연령</span>
-            <span>{performanceDetail.age}</span>
-          </div>
+          {/* 공연 정보 요약 */}
+          {renderPerformanceInfoList()}
         </div>
       </div>
     </>
