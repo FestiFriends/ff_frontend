0800c279288c8074d8dbd738505ffe65ddbdc37c
songdaramji <songdaramji@gmail.com>
2025-06-08 23:00:40 +0900

    refactor: HashtagBadge 컴포넌트 prop 제거 및 스타일링 수정

--- src/components/common/HashtagBadge/HashtagBadge.test.tsx
+++ src/components/common/HashtagBadge/HashtagBadge.test.tsx
@@ -3,56 +3,16 @@ import HashtagBadge from './HashtagBadge';
 
 describe('HashtagBadge 컴포넌트 테스트', () => {
   test('배지 및 text가 화면에 정상적으로 렌더링된다', () => {
-    render(
-      <HashtagBadge
-        text='펜타포트'
-        type='userProfile'
-      />
-    );
+    render(<HashtagBadge text='펜타포트' />);
     const badge = screen.getByText('펜타포트');
     expect(badge).toBeInTheDocument();
   });
 
   //   Todo: 스타일 변경에 따른 리팩토링 필요
-  test('type props에 따라 올바른 클래스가 적용된다', () => {
-    render(
-      <>
-        <HashtagBadge
-          text='groupInfo'
-          type='groupInfo'
-        />
-        <HashtagBadge
-          text='groupCard'
-          type='groupCard'
-        />
-        <HashtagBadge
-          text='userProfile'
-          type='userProfile'
-        />
-      </>
-    );
-    const groupInfo = screen.getByText('groupInfo');
-    const groupCard = screen.getByText('groupCard');
-    const userProfile = screen.getByText('userProfile');
-    expect(groupInfo).toBeInTheDocument();
-    expect(groupCard).toBeInTheDocument();
-    expect(userProfile).toBeInTheDocument();
-    expect(groupInfo).toHaveClass(
-      'cursor-pointer rounded-full bg-yellow-200 px-4 py-2 font-semibold text-gray-800 select-none'
-    );
-    expect(groupCard).toHaveClass(
-      'cursor-pointer rounded-lg bg-blue-100 px-4 py-2 font-medium text-blue-600 select-none'
-    );
-    expect(userProfile).toHaveClass(
-      'cursor-pointer rounded-lg bg-purple-100 px-4 py-2 font-semibold text-purple-600 select-none'
-    );
-  });
-
   test('className props를 할당했을 때 올바른 클래스가 적용된다', () => {
     render(
       <HashtagBadge
         text='펜타포트'
-        type='userProfile'
         className='font-extrabold'
       />
     );
@@ -66,7 +26,6 @@ describe('HashtagBadge 컴포넌트 테스트', () => {
     render(
       <HashtagBadge
         text='클릭'
-        type='groupInfo'
         isClickable={true}
         onClick={handleClick}
       />
@@ -82,7 +41,6 @@ describe('HashtagBadge 컴포넌트 테스트', () => {
     render(
       <HashtagBadge
         text='클릭'
-        type='groupInfo'
         isClickable={false}
         onClick={handleClick}
       />
@@ -97,7 +55,6 @@ describe('HashtagBadge 컴포넌트 테스트', () => {
     render(
       <HashtagBadge
         text='클릭'
-        type='groupInfo'
         isClickable={true}
       />
     );
