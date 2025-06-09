42a0e535952d2b0ebc61e9a668f4e97dfa3825b3
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2025-06-09 09:10:23 +0900

    [Feature/issue-244] Pagination 컴포넌트 구현, 테스트 및 문서 작성 (#245)
    * feat: Pagination 공용 컴포넌트 구현
    
    * design: Pagination 컴포넌트 디자인 적용
    
    * fix: button에 aria-label 추가
    
    * test: Pagination 컴포넌트 테스트 코드 작성
    
    * docs: Pagination 컴포넌트 리드미 문서 작성
    
    * docs: 오타 수정
    
    * chore: 안쓰는 주석 제거
    
    * design: Pagination 버튼 text 스타일 수정

---
+++ src/components/common/Pagination/Pagination.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import ChevronLeftIcon from '@/components/icons/ChevronLeftIcon';
+import ChevronRightIcon from '@/components/icons/ChevronRightIcon';
+import { cn } from '@/lib/utils';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+  maxVisiblePages?: number;
+  className?: string;
+}
+
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  maxVisiblePages = 5,
+  className,
+}: PaginationProps) => {
+  const rangeStart =
+    Math.floor((currentPage - 1) / maxVisiblePages) * maxVisiblePages + 1;
+  const rangeEnd = Math.min(rangeStart + maxVisiblePages - 1, totalPages);
+
+  const getPageNumbers = () =>
+    Array.from({ length: rangeEnd - rangeStart + 1 }, (_, i) => rangeStart + i);
+
+  const goToPrevPage = () => {
+    if (currentPage === 1) return;
+    const prevPage = currentPage - 1;
+    onPageChange(prevPage);
+  };
+
+  const goToNextPage = () => {
+    if (currentPage === totalPages) return;
+    const nextPage = currentPage + 1;
+    onPageChange(nextPage);
+  };
+
+  return (
+    <div className={cn('flex items-center justify-center gap-1', className)}>
+      <button
+        onClick={goToPrevPage}
+        aria-label='prev page button'
+        className={cn(
+          'flex aspect-square h-10 w-10 items-center justify-center px-2 py-1.5 text-gray-950 transition-all',
+          currentPage === 1 && 'text-gray-300'
+        )}
+      >
+        <ChevronLeftIcon />
+      </button>
+
+      {getPageNumbers().map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={
+            page === currentPage
+              ? 'flex aspect-square h-10 w-10 flex-col items-center justify-center border-b-2 border-b-primary-red p-2 text-center text-14_B leading-[171.429%] tracking-[-0.35px] text-gray-950 transition-all select-none'
+              : 'flex aspect-square h-10 w-10 flex-col items-center justify-center p-2 text-center text-14_M leading-[171.429%] tracking-[-0.35px] text-gray-300 transition-all select-none'
+          }
+          aria-label={`${page} page button`}
+          aria-current={page === currentPage ? 'page' : undefined}
+        >
+          {page}
+        </button>
+      ))}
+
+      <button
+        onClick={goToNextPage}
+        aria-label='next page button'
+        className={cn(
+          'flex aspect-square h-10 w-10 items-center justify-center px-2 py-1.5 text-gray-950 transition-all',
+          currentPage === totalPages && 'text-gray-300'
+        )}
+      >
+        <ChevronRightIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/components/pages/performanceDetail/index.ts
+++ src/components/pages/performanceDetail/index.ts
@@ -2,7 +2,6 @@ export { default as Wrapper } from './PerformanceDetailWrapper';
 export { default as Groups } from './PerformanceDetailGroups';
 export { default as GroupsOptionTabs } from './PerformanceDetailGroupsOptionTabs';
 export { default as GroupsList } from './PerformanceDetailGroupsList';
-export { default as GroupsPagination } from './PerformanceDetailGroupsPagination';
 export { default as Info } from './PerformanceDetailInfo';
 export { default as Summary } from './PerformanceDetailSummary';
 export { default as Tabs } from './PerformanceDetailTabs';
