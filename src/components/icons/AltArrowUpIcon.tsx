8972402236e240b9f037298df6b461290c60a5a3
Seokyung <xuejd17@gmail.com>
2025-06-09 15:30:46 +0900

    chore: 아이콘 추가

---
+++ src/components/icons/AltArrowUpIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AltArrowUpIcon = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    xmlns='http://www.w3.org/2000/svg'
+    width='16'
+    height='17'
+    viewBox='0 0 16 17'
+    fill='none'
+  >
+    <path
+      d='M8.48171 7.18465C8.24078 6.91762 7.7592 6.91762 7.51827 7.18465L4.48531 10.5462C4.18731 10.8765 4.4706 11.348 4.96703 11.348H11.033C11.5294 11.348 11.8127 10.8765 11.5147 10.5462L8.48171 7.18465Z'
+      fill='currentColor'
+    />
+  </svg>
+);
+
+export default AltArrowUpIcon;
