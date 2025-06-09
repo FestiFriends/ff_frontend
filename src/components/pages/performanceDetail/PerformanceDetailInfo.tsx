cbebc2b385e4e2ed07c691df1a8a75c783ec0fa8
Seokyung <xuejd17@gmail.com>
2025-06-09 11:23:40 +0900

    design: PerformanceDetailInfo 컴포넌트 skeleton 설정

--- src/components/pages/performanceDetail/PerformanceDetailInfo.tsx
+++ src/components/pages/performanceDetail/PerformanceDetailInfo.tsx
@@ -1,22 +1,30 @@
 import Poster from '@/components/common/poster/Poster';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Image } from '@/types/image';
 import { Performance } from '@/types/performance';
 
 interface PerformanceDetailInfoProps {
-  performanceDetail: Performance;
+  isPending: boolean;
+  performanceDetail?: Performance;
 }
 
 const PerformanceDetailInfo = ({
+  isPending,
   performanceDetail,
 }: PerformanceDetailInfoProps) => (
-  <div className='w-full'>
-    {performanceDetail?.images?.map((image) => (
-      <Poster
-        key={image.id}
-        src={image.src}
-        alt={image.alt || ''}
-        className='h-100 w-full'
-      />
-    ))}
+  <div className='flex w-full flex-col gap-5 px-4 pb-5'>
+    {isPending ? (
+      <Skeleton className='h-[60vh] w-full' />
+    ) : (
+      performanceDetail?.images?.map((image: Image) => (
+        <Poster
+          key={image.id}
+          src={image.src}
+          alt={image.alt || ''}
+          className='aspect-[2/3] h-auto w-full'
+        />
+      ))
+    )}
   </div>
 );
 
