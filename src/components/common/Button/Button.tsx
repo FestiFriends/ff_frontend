3dc80a8abd1347131105b997aadb78c37427a328
songdaramji <songdaramji@gmail.com>
2025-06-10 20:28:46 +0900

    fix: 버튼 styles 분리, 프로필 이미지 사이즈 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,24 +1,20 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import { cn } from '@/lib/utils';
-
+import { buttonStyles } from './Button.styles';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?:
-    | 'normalPrimary'
-    | 'normalSecondary'
-    | 'labelPrimary'
-    | 'labelSecondary'
-    | 'disablePrimary'
-    | 'disableSecondary';
-  size?: 'sm' | 'md' | 'lg';
+  variant?: keyof typeof buttonStyles.variants;
+  color?: keyof typeof buttonStyles.variants.primary; // color는 primary와 secondary 안에 있는 값들만 가능
+  size?: keyof typeof buttonStyles.sizes;
   disabled?: boolean;
   className?: string;
-  onClick?: () => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = ({
-  variant = 'normalPrimary',
-  size = 'md',
-  disabled,
+  variant = 'primary',
+  color = 'normal',
+  size = 'full',
+  disabled = false,
   className,
   onClick,
   children,
@@ -26,29 +22,11 @@ const Button = ({
 }: PropsWithChildren<ButtonProps>) => {
   const buttonClasses = cn(
     // default style
-    'inline-flex h-[42px] w-full cursor-pointer items-center justify-center rounded-12 text-center text-14_M',
+    'inline-flex cursor-pointer items-center justify-center rounded-12 text-center text-14_M',
 
-    // variant style
-    {
-      'bg-primary-red text-white': variant === 'normalPrimary',
-      'border border-primary-red text-primary-red':
-        variant === 'normalSecondary',
-      'bg-[#FFEEEF] text-primary-red': variant === 'labelPrimary',
-      'border border-primary-100 text-primary-red':
-        variant === 'labelSecondary',
-      'bg-gray-200 text-gray-100': variant === 'disablePrimary',
-      'border border-gray-200 text-gray-100': variant === 'disableSecondary',
-    },
+    buttonStyles.variants[variant]?.[color],
 
-    // size style
-    {
-      'px-2 py-1': size === 'sm',
-      'px-5 py-2.5': size === 'md',
-      'px-8 py-2': size === 'lg',
-    },
-
-    // disabled style
-    disabled && 'cursor-not-allowed border-none bg-gray-200 text-white',
+    buttonStyles.sizes[size],
 
     // custom style
     className
