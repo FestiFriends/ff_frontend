23a27e17e76623d8f1d4bf07d0379ccd364accf1
jadewisemann <jadewisemann@gmail.com>
2025-06-11 09:12:13 +0900

    feat: 퍼포먼스 페이지 sort drop 다운 내용 변경

--- src/app/(hasGNB)/performances/page.tsx
+++ src/app/(hasGNB)/performances/page.tsx
@@ -2,23 +2,52 @@ import {
   PerformanceListContainer,
   Search,
   SortDropdown,
-  Filter,
   DatePicker,
 } from '@/components/pages/performances';
+import { LocationLabels } from '@/constants/locationLabels';
+import { LocationValues } from '@/constants/locationValues';
+import { Location } from '@/types/enums';
+
+const DROPDOWN_OPTIONS = {
+  SORT: {
+    placeholder: '정렬',
+    queryKey: 'sort',
+    data: [
+      { label: '최신순', value: 'latest' },
+      { label: '인기순', value: 'popular' },
+      { label: '이름순', value: 'name' },
+    ],
+  },
+  CATEGORY: {
+    placeholder: '유형',
+    queryKey: 'visit',
+    data: [
+      { label: '내한', value: '내한' },
+      { label: '국내', value: '국내' },
+    ],
+  },
+  LOCATION: {
+    placeholder: '위치',
+    queryKey: 'location',
+    data: Object.values(Location).map((locationKey) => ({
+      label: `${LocationLabels[locationKey]}`,
+      value: `${LocationValues[locationKey]}`,
+    })),
+  },
+};
 
 const PerformancesPage = () => (
   <>
     <Search />
     <div className='flex'>
-      <SortDropdown
-        queryKey='sort'
-        placeholder='정렬'
-      />
-      <SortDropdown
-        queryKey='category'
-        placeholder='유형'
-      />
-      <Filter />
+      {Object.values(DROPDOWN_OPTIONS).map((option) => (
+        <SortDropdown
+          key={option.queryKey}
+          queryKey={option.queryKey}
+          placeholder={option.placeholder}
+          options={option.data}
+        />
+      ))}
       <DatePicker />
     </div>
     <PerformanceListContainer />
