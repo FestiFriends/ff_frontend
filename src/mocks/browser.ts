b7854a960bca12f50d7361cee57c9ec95a61e09e
Jade Wisemann <139527412+jadewisemann@users.noreply.github.com>
2025-06-11 17:29:22 +0900

    [Feature/issue-193] msw 설정 (#294)
    * feat: msw 헨들러 추가
    
    * chore:  performance 핸들러 중복 선언 제거
    
    * feat: 찜한 공연 핸들러 추가
    
    * chore: performanc 핸들러 데이터 분리
    
    * feat: usersHandler 추가
    
    * feat: useFavorite 훅 추가
    
    * chore: 문제 수정

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -4,11 +4,13 @@ import { notificationHandlers } from './handlers/notificationHandlers';
 import { performancesHandlers } from './handlers/performancesHandlers';
 import { profileHandlers } from './handlers/profileHandlers';
 import { reviewsHandlers } from './handlers/reviewsHandlers';
+import { usersHandlers } from './handlers/usersHandlers';
 
 export const worker = setupWorker(
   ...profileHandlers,
   ...performancesHandlers,
   ...reviewsHandlers,
   ...notificationHandlers,
-  ...groupsHandlers
+  ...groupsHandlers,
+  ...usersHandlers
 );
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,9 +2,11 @@ import { setupServer } from 'msw/node';
 import { groupsHandlers } from './handlers/groupsHandlers';
 import { performancesHandlers } from './handlers/performancesHandlers';
 import { reviewsHandlers } from './handlers/reviewsHandlers';
+import { usersHandlers } from './handlers/usersHandlers';
 
 export const server = setupServer(
-  ...reviewsHandlers,
+  ...usersHandlers,
   ...performancesHandlers,
+  ...reviewsHandlers,
   ...groupsHandlers
 );
