ebe6172d3e473521c2ae002f8c774545a4025e61
SeongIn Ko <jjjk0605@naver.com>
2025-06-12 10:40:04 +0900

    feat: 알림 데이터 타입 변경 및 msw 데이터 수정

--- src/types/notification.ts
+++ src/types/notification.ts
@@ -1,9 +1,28 @@
 import { ApiResponse, CursorResponse } from './api';
 
+export type NotificationTargetType =
+  | null
+  | { postId: string; groupId: string }
+  | { groupId: string };
+
+export type NotificationType =
+  | 'APPLICATION'
+  | 'APPLIED'
+  | 'POST'
+  | 'GROUP'
+  | 'SCHEDULE'
+  | 'REVIEW'
+  | 'MY_PROFILE'
+  | 'REJECTED'
+  | 'BANNED';
+
 export interface NotificationData {
   id: string; // 알림 ID
   message: string; // 알림 내용
+  type: NotificationType;
+  target: NotificationTargetType;
   createdAt: string; // 알림 생성 시간 (ISO 8601)
+  isRead: boolean;
 }
 
 export type GetNotificationsResponse = ApiResponse<NotificationData[]>
