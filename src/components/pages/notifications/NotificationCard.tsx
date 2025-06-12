6db8717a563e084f1d1d4cefe1315dfc83efdaee
SeongIn Ko <jjjk0605@naver.com>
2025-06-12 10:42:19 +0900

    feat: 알림 개별 읽음 적용 및 라우팅 기능 추가

--- src/components/pages/notifications/NotificationCard.tsx
+++ src/components/pages/notifications/NotificationCard.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import { useDeleteNotifications } from '@/hooks/notificationHooks/notificationHooks';
-import { NotificationData } from '@/types/notification';
+import React, { MouseEvent } from 'react';
+import { useRouter } from 'next/navigation';
+import {
+  useDeleteNotifications,
+  usePatchReadNotifications,
+} from '@/hooks/notificationHooks/notificationHooks';
+import {
+  NotificationData,
+  NotificationTargetType,
+  NotificationType,
+} from '@/types/notification';
+
+const getNotificationHref = (
+  type: NotificationType,
+  target: NotificationTargetType
+): string => {
+  switch (type) {
+    case 'APPLICATION':
+      return '/groups/managements';
+    case 'APPLIED':
+      return '/groups/managements';
+    case 'REVIEW':
+      return '/reviews/managements';
+    case 'MY_PROFILE':
+      return '/profiles/me';
+    case 'REJECTED':
+    case 'BANNED':
+      return '';
+    case 'GROUP':
+    case 'SCHEDULE':
+      if (target && 'groupId' in target) {
+        return `/groups/${target.groupId}`;
+      }
+      return '';
+    case 'POST':
+      if (target && 'groupId' in target && 'postId' in target) {
+        return `/groups/${target.groupId}/posts/${target.postId}`;
+      }
+      return '';
+  }
+};
 
 interface NotificationCardProps {
   notification: NotificationData;
 }
 
 const NotificationCard = ({ notification }: NotificationCardProps) => {
-  const { mutate, isPending } = useDeleteNotifications();
+  const router = useRouter();
+  const { mutate: deleteMutate, isPending: deleteIsPending } =
+    useDeleteNotifications();
 
-  const handleDelete = () => {
-    if (isPending) return;
+  const { mutateAsync: readMutate, isPending: readIsPending } =
+    usePatchReadNotifications();
 
-    mutate(notification.id);
+  const handleDelete = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (deleteIsPending) return;
+
+    deleteMutate(notification.id);
   };
 
-  if (isPending) return null;
+  const handleRead = async () => {
+    if (readIsPending) return;
+
+    await readMutate(notification.id);
+  };
+
+  if (deleteIsPending) return null;
+
+  const href = getNotificationHref(notification.type, notification.target);
 
   return (
     <div className='flex h-10 items-center justify-between bg-gray-100 px-4'>
-      {notification.message}
-      <button onClick={handleDelete}>삭제</button>
+      <button
+        className='h-full w-full'
+        onClick={() => {
+          handleRead();
+          router.push(href);
+        }}
+      >
+        {notification.message}
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          handleRead();
+        }}
+        disabled={notification.isRead}
+        className='shrink-0 bg-blue-200 disabled:bg-gray-200'
+      >
+        읽음
+      </button>
+      <button
+        onClick={(e) => handleDelete(e)}
+        className='shrink-0 bg-red-200'
+      >
+        삭제
+      </button>
     </div>
   );
 };
