0d07c2b933dea33b41ae33374bd529d607f1ff66
Seokyung <xuejd17@gmail.com>
2025-06-12 22:45:59 +0900

    test: 웹 소켓 연결 및 통신 확인용 컴포넌트 추가

---
+++ src/hooks/chatHooks/chatHooks.ts
@@ -0,0 +1,127 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { Client } from '@stomp/stompjs';
+// import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
+// import { AxiosResponse } from 'axios';
+import SockJS from 'sockjs-client';
+import { getAccessToken } from '@/lib/apiFetcher';
+// import { CHAT_QUERY_KEY } from '@/constants/queryKeys';
+// import { chatServiceApi } from '@/services/chatService';
+// import { ApiResponse, CursorRequest } from '@/types/api';
+// import { GetChatMessageListResponse } from '@/types/chat';
+
+/**
+ * 채팅 웹소켓 연결
+ * @param chatRoomId
+ * @returns { messages, sendMessage, isConnected }
+ */
+export const useChatWebSocket = (chatRoomId: string | null) => {
+  const [client, setClient] = useState<Client | null>(null);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const accessToken = getAccessToken();
+    if (!accessToken || !chatRoomId) return;
+
+    const socket = new SockJS(`${process.env.NEXT_PUBLIC_BACKEND_URL}/chat`);
+
+    const stompClient = new Client({
+      webSocketFactory: () => socket,
+      connectHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      onConnect: () => {
+        setIsConnected(true);
+        console.log('connected to stomp client');
+
+        stompClient.subscribe(`/sub/chat/${chatRoomId}`, (message) => {
+          console.log(`subscribed chat room ${chatRoomId}`);
+          const body = JSON.parse(message.body);
+          console.log('received message: ', body);
+          setMessages((prev) => [...prev, body]);
+        });
+      },
+      onDisconnect: () => {
+        setIsConnected(false);
+        console.log('disconnected to stomp client');
+      },
+      debug: (debugMessage: string) => {
+        console.log(`debug: ${debugMessage}`);
+      },
+      onStompError: (error) => {
+        console.log(`stomp error: ${error}`);
+      },
+      onWebSocketError: (error) => {
+        console.log(`web socket error: ${error}`);
+      },
+      reconnectDelay: 3000,
+    });
+
+    stompClient.activate();
+    setClient(stompClient);
+
+    return () => {
+      stompClient.deactivate();
+      setClient(null);
+      setIsConnected(false);
+    };
+  }, [chatRoomId]);
+
+  const sendMessage = (content: string) => {
+    if (!client || !isConnected) return;
+
+    try {
+      client.publish({
+        destination: `/pub/chat/${chatRoomId}`,
+        body: JSON.stringify(content),
+      });
+      console.log('send message', content);
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
+  return { messages, sendMessage, isConnected };
+};
+
+// export const useGetChatMessageList = (size: CursorRequest['size']) =>
+//   useInfiniteQuery<
+//     AxiosResponse<GetChatMessageListResponse>,
+//     ApiResponse,
+//     InfiniteData<AxiosResponse<GetChatMessageListResponse>>,
+//     string[],
+//     number | undefined
+//   >({
+//     queryKey: [CHAT_QUERY_KEY.chat],
+//     queryFn: ({ pageParam }) =>
+//       chatServiceApi.getChatMessageList({
+//         // chatRoomId,
+//         cursorId: pageParam,
+//         size,
+//       }),
+//     initialPageParam: undefined,
+//     getNextPageParam: (lastPage) =>
+//       lastPage.data.hasNext ? lastPage.data.cursorId : undefined,
+//   });
+
+// export const useGetChatMessageList = (size: CursorRequest['size']) =>
+//   useInfiniteQuery<
+//     AxiosResponse<GetChatMessageListResponse>,
+//     ApiResponse,
+//     InfiniteData<AxiosResponse<GetChatMessageListResponse>>,
+//     string[],
+//     number | undefined
+//   >({
+//     queryKey: [CHAT_QUERY_KEY.chat],
+//     queryFn: ({ pageParam }) =>
+//       chatServiceApi.getChatMessageList({
+//         // chatRoomId,
+//         cursorId: pageParam,
+//         size,
+//       }),
+//     initialPageParam: undefined,
+//     getNextPageParam: (lastPage) =>
+//       lastPage.data.hasNext ? lastPage.data.cursorId : undefined,
+//   });
