6df659ca134db66e4d6bf344b4b585324087a5f2
Seokyung <xuejd17@gmail.com>
2025-06-12 22:47:53 +0900

    feat: Chat 컴포넌트 생성

--- src/components/pages/groupDetail/Chat/Chat.tsx
+++ src/components/pages/groupDetail/Chat/Chat.tsx
@@ -1,15 +1,20 @@
-import ChatArea from './ChatArea';
-import ChatInfo from './ChatInfo';
-import ChatNotice from './ChatNotice';
+// import ChatArea from './ChatArea';
+// import ChatInfo from './ChatInfo';
+// import ChatNotice from './ChatNotice';
+// import ChatRoom from './ChatRoom';
 
 const Chat = () => (
-  <div className='flex flex-col gap-5 px-4 pb-4'>
-    <div>
-      <ChatInfo />
-      <ChatNotice />
-    </div>
-    <ChatArea />
-  </div>
+  <div>채팅</div>
+  // <div>
+  //   <ChatRoom />
+  // </div>
+  // <div className='flex flex-col gap-5 px-4 pb-4'>
+  //   <div>
+  //     <ChatInfo />
+  //     <ChatNotice />
+  //   </div>
+  //   <ChatArea />
+  // </div>
 );
 
 export default Chat;
