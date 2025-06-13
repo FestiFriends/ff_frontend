1ae92c8cd3912719dad09fa3f6bc25d58072ecf6
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2025-06-13 17:29:42 +0900

    [Feature/issue-311] suspensive 라이브러리 도입 (#323)
    * chore: suspensive 라이브러리 설치
    
    * feat: useQuery함수를 queryOptions를 반환하는 함수로 변경
    
    * feat: 메인 페이지 공연 카드 스켈레톤 컴포넌트 분리
    
    * refactor: suspensive를 사용하여 pending, error처리하도록 리팩토링
    
    * refactor: 리뷰 관련 api의 응답이 AxiosResponse로 감싸지지 않도록 변경
    
    * refactor: 리뷰 관련 무한 스크롤을 suspensive를 사용하여 error, pending처리하도록 리팩토링
    
    * fix: 리뷰 무한스크롤 size가 1인 오류 수정
    
    * fix: 리뷰 작성 관련 낙관적 업데이트 오류 수정
    
    * refactor: console.log삭제
    
    * refactor: 알림과 리뷰 무한 스크롤 관련 응답값의 data가 옵셔널이 아니도록 변경
    
    * feat: 알림 조회 useQuery를 삭제하고 옵션을 반환하는 함수로 변경 및 전체삭제 낙관적 업데이트 수정
    
    * feat: 무한 스크롤용 suspensive를 사용하는 공통 컴포넌트 생성
    
    * refactor: 리뷰 관련 무한스크롤을 InfiniteReviewList컴포넌트를 사용하는 코드로 변경
    
    * feat: 알림 카드 스켈레톤 컴포넌트 분리
    
    * feat: 알림 관련 무한스크롤을 InfiniteReviewList컴포넌트를 사용하는 코드로 변경
    
    * chore: npm run dev turbo옵션 추가
    
    * fix: 무한스크롤 pending시 스크롤을 움직이면 api가 중복호출 되던 오류 해결
    
    * rename: InfiniteList로 이름 변경
    
    * rename: InfiniteList로 이름 변경
    
    * fix: 무한스크롤 size설정 제거

--- src/components/pages/main/MainTopGroupsPerformances.tsx
+++ src/components/pages/main/MainTopGroupsPerformances.tsx
@@ -1,26 +1,14 @@
 'use client';
 import React from 'react';
-import { useGetTopByGroupCountPerformances } from '@/hooks/performanceHooks/performanceHooks';
+import { getTopByGroupCountQueryOptions } from '@/hooks/performanceHooks/performanceHooks';
 import PerformanceWrapper from './PerformanceWrapper';
 
-const MainTopGroupsPerformances = () => {
-  const {
-    data: topByGroupCountPerformances,
-    isPending,
-    isError,
-    error,
-  } = useGetTopByGroupCountPerformances();
-
-  return (
-    <PerformanceWrapper
-      href='/performances'
-      title='모임 많은 공연'
-      isPending={isPending}
-      isError={isError}
-      error={error}
-      performances={topByGroupCountPerformances}
-    />
-  );
-};
+const MainTopGroupsPerformances = () => (
+  <PerformanceWrapper
+    href='/performances'
+    title='모임 많은 공연'
+    queryOptions={getTopByGroupCountQueryOptions()}
+  />
+);
 
 export default MainTopGroupsPerformances;
--- src/components/pages/main/PerformanceWrapper.tsx
+++ src/components/pages/main/PerformanceWrapper.tsx
@@ -1,97 +1,72 @@
+import { ErrorBoundary, Suspense } from '@suspensive/react';
+import { SuspenseQuery } from '@suspensive/react-query';
+import { UseSuspenseQueryOptions } from '@tanstack/react-query';
 import Link from 'next/link';
 import PerformanceCard from '@/components/common/PerformanceCard';
-// import MainPerformanceCard from './MainPerformanceCard';
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from '@/components/ui/carousel';
-import { Skeleton } from '@/components/ui/skeleton';
-import { ApiResponse } from '@/types/api';
 import { PerformancesResponse } from '@/types/performance';
+import MainPerformanceSkeletonCard from './MainPerformanceSkeletonCard';
 
 interface PerformanceWrapperProps {
   title: string;
   href: string;
-  isPending: boolean;
-  isError: boolean;
-  error: ApiResponse | null;
-  performances?: PerformancesResponse;
+  queryOptions: UseSuspenseQueryOptions<PerformancesResponse>;
 }
 
 const PerformanceWrapper = ({
   title,
   href,
-  isPending,
-  isError,
-  error,
-  performances,
-}: PerformanceWrapperProps) => {
-  let content;
-
-  if (isPending) {
-    content = Array.from({ length: 5 }).map((_, index) => (
-      <CarouselItem
-        key={index}
-        className='basis-[150px] p-0'
+  queryOptions,
+}: PerformanceWrapperProps) => (
+  <div className='flex flex-col gap-5 bg-white px-4 pt-5 pb-[30px]'>
+    <div className='flex items-center justify-between'>
+      <h2 className='flex h-[19px] items-center text-16_B text-gray-950'>
+        {title}
+      </h2>
+      <Link
+        href={href}
+        className='flex h-[17px] items-center text-14_M text-gray-500 underline'
       >
-        <div className='flex w-[150px] flex-col gap-3'>
-          <Skeleton className='h-[200px] w-[150px] rounded-[12px]' />
-          <div className='flex flex-col gap-2'>
-            <Skeleton className='h-[19px] w-[150px]' />
-            <Skeleton className='h-[17px] w-[150px]' />
-          </div>
-        </div>
-      </CarouselItem>
-    ));
-  }
-
-  if (isError) {
-    content = <p>{error?.message}</p>;
-  }
-
-  if (performances?.data?.length === 0) {
-    content = <p>데이터가 존재하지 않습니다.</p>;
-  }
-
-  if (performances?.data && performances.data.length > 0) {
-    content = performances.data.map((performance, idx) => (
-      <CarouselItem
-        key={performance.id}
-        className='basis-[150px] p-0'
-      >
-        <PerformanceCard
-          ranking={idx + 1}
-          performance={performance}
-        />
-      </CarouselItem>
-    ));
-  }
-
-  return (
-    <div className='flex flex-col gap-5 bg-white px-4 pt-5 pb-[30px]'>
-      <div className='flex items-center justify-between'>
-        <h2 className='flex h-[19px] items-center text-16_B text-gray-950'>
-          {title}
-        </h2>
-        <Link
-          href={href}
-          className='flex h-[17px] items-center text-14_M text-gray-500 underline'
-        >
-          더보기
-        </Link>
-      </div>
-
-      <Carousel
-        opts={{
-          align: 'start',
-        }}
-        className='w-full'
-      >
-        <CarouselContent className='z-10 m-0 gap-5'>{content}</CarouselContent>
-      </Carousel>
+        더보기
+      </Link>
     </div>
-  );
-};
 
+    <Carousel
+      opts={{
+        align: 'start',
+      }}
+      className='w-full'
+    >
+      <CarouselContent className='z-10 m-0 gap-5'>
+        <ErrorBoundary fallback={({ error }) => <p>{error.message}</p>}>
+          <Suspense fallback={<MainPerformanceSkeletonCard />}>
+            <SuspenseQuery {...queryOptions}>
+              {({ data }) => {
+                if (data.data?.length === 0) {
+                  return <p>데이터가 존재하지 않습니다.</p>;
+                } else {
+                  return data.data?.map((performance, idx) => (
+                    <CarouselItem
+                      key={performance.id}
+                      className='basis-[150px] p-0'
+                    >
+                      <PerformanceCard
+                        ranking={idx + 1}
+                        performance={performance}
+                      />
+                    </CarouselItem>
+                  ));
+                }
+              }}
+            </SuspenseQuery>
+          </Suspense>
+        </ErrorBoundary>
+      </CarouselContent>
+    </Carousel>
+  </div>
+);
 export default PerformanceWrapper;
