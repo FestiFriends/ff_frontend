943b998dbfebf074e6573843b4598edaa3a7410a
Seokyung <xuejd17@gmail.com>
2025-06-13 17:53:59 +0900

    feat: 모임 상세 페이지 파일 생성

---
+++ src/app/(hasGNB)/(isLogin)/groups/[groupId]/page.tsx
@@ -0,0 +1,10 @@
+import Chat from '@/components/pages/groupDetail/Chat/Chat';
+
+const GroupDetailPage = () => (
+  <div>
+    모임 상세 페이지
+    <Chat />
+  </div>
+);
+
+export default GroupDetailPage;
