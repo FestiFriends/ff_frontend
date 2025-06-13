16b4388c6f35d9db92c310cb700c0ffff2a919fc
Seokyung <xuejd17@gmail.com>
2025-06-13 09:43:35 +0900

    fix: 모임 목록 조회 api 파라미터들 params 객체로 묶음

--- src/services/groupsService.ts
+++ src/services/groupsService.ts
@@ -1,32 +1,15 @@
+import * as qs from 'qs';
 import apiFetcher from '@/lib/apiFetcher';
 import { GetGroupsParams } from '@/types/group';
 import { PerformanceGroupsApiResponse } from '@/utils/formatGroupCardData';
 
 export const groupsApi = {
-  getGroups: async ({
-    performanceId,
-    page,
-    size,
-    sortType,
-    category,
-    startDate,
-    endDate,
-    location,
-    gender,
-  }: GetGroupsParams) =>
-    await apiFetcher.get<PerformanceGroupsApiResponse>(
-      `/api/v1/performances/${performanceId}/groups`,
-      {
-        params: {
-          page,
-          size,
-          sortType,
-          category,
-          startDate,
-          endDate,
-          location,
-          gender,
-        },
-      }
-    ),
+  getGroups: async (params: GetGroupsParams) => {
+    const { performanceId, ...rest } = params;
+    const queryString = qs.stringify({ ...rest }, { skipNulls: true });
+
+    return await apiFetcher.get<PerformanceGroupsApiResponse>(
+      `/api/v1/performances/${performanceId}/groups?${queryString}`
+    );
+  },
 };
