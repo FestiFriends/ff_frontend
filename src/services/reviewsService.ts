ea28177c9fcadc50fa5d6daf30888b93950e05a0
SeongIn Ko <jjjk0605@naver.com>
2025-06-13 11:09:01 +0900

    refactor: 리뷰 관련 api의 응답이 AxiosResponse로 감싸지지 않도록 변경

--- src/services/reviewsService.ts
+++ src/services/reviewsService.ts
@@ -8,14 +8,21 @@ import {
 
 export const reviewsApi = {
   getWrittenReviews: async ({ cursorId, size = 20 }: CursorRequest) =>
-    await apiFetcher.get<WrittenReviewsResponse>('/api/v1/reviews/written', {
-      params: { cursorId, size },
-    }),
+    (
+      await apiFetcher.get<WrittenReviewsResponse>('/api/v1/reviews/written', {
+        params: { cursorId, size },
+      })
+    ).data,
 
   getWritableReviews: async ({ cursorId, size = 20 }: CursorRequest) =>
-    await apiFetcher.get<WritableReviewsResponse>('/api/v1/reviews/writable', {
-      params: { cursorId, size },
-    }),
+    (
+      await apiFetcher.get<WritableReviewsResponse>(
+        '/api/v1/reviews/writable',
+        {
+          params: { cursorId, size },
+        }
+      )
+    ).data,
 
   postWriteReview: async (data: PostReviewRequest) =>
     await apiFetcher.post<ApiResponse, ApiResponse, PostReviewRequest>(
