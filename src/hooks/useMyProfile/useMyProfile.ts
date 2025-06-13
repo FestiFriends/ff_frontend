a02a3f91f83237135bf0b065a0ca961c152fc3ec
CHAN-H0 <lch0309k@gmail.com>
2025-06-13 10:51:34 +0900

    fix: useMyProfile 변경

--- src/hooks/useMyProfile/useMyProfile.ts
+++ src/hooks/useMyProfile/useMyProfile.ts
@@ -2,9 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import { profilesApi } from '@/services/profileService';
 import { FullProfile } from '@/types/profiles';
 
-export const useProfile = (userId: string) =>
+export const useMyProfile = () =>
   useQuery<FullProfile, Error>({
-    queryKey: ['profile', userId] as const,
+    queryKey: ['myProfile'] as const,
     queryFn: () => profilesApi.getProfile('me'),
     staleTime: 1000 * 60 * 5,
     retry: 0,
