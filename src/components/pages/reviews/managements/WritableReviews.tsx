3cc9b3dd670dd7f04d629c02edf4200a42798185
SeongIn Ko <jjjk0605@naver.com>
2025-06-13 16:04:37 +0900

    refactor: 리뷰 관련 무한스크롤을 InfiniteReviewList컴포넌트를 사용하는 코드로 변경

--- src/components/pages/reviews/managements/WritableReviews.tsx
+++ src/components/pages/reviews/managements/WritableReviews.tsx
@@ -1,65 +1,34 @@
 'use client';
 
 import React from 'react';
-import { ErrorBoundary, Suspense } from '@suspensive/react';
-import { SuspenseInfiniteQuery } from '@suspensive/react-query';
-import {
-  InfiniteData,
-  UseSuspenseInfiniteQueryResult,
-} from '@tanstack/react-query';
+import InfiniteReviewList from '@/components/common/InfiniteReviewList';
 import { infiniteWritableReviewsOptions } from '@/hooks/reviewHooks/reviewHooks';
-import { useInfiniteScroll } from '@/hooks/useInfiniteScroll/useInfiniteScroll';
-import { ApiResponse } from '@/types/api';
 import { WritableReviewsResponse } from '@/types/reviews';
 import ReviewCard from './ReviewCard';
 import ReviewCardSkeleton from './ReviewCardSkeleton';
 import WritableReviewsContent from './WritableReviewsContent';
 
-const ReviewList = ({
-  data,
-  fetchNextPage,
-  hasNextPage,
-  isFetchingNextPage,
-}: UseSuspenseInfiniteQueryResult<
-  InfiniteData<WritableReviewsResponse, unknown>,
-  ApiResponse
->) => {
-  const bottomRef = useInfiniteScroll<HTMLDivElement>(
-    fetchNextPage,
-    hasNextPage
-  );
-
-  return (
-    <div className='flex flex-col items-center gap-5'>
-      {data.pages.map((page) =>
-        page.data?.map((item) => (
-          <ReviewCard
-            key={item.groupId}
-            groupInfo={item}
-            reviewsCount={item.reviews.length}
-            content={
-              <WritableReviewsContent
-                reviews={item.reviews}
-                groupId={item.groupId}
-              />
-            }
-          />
-        ))
-      )}
-      <div ref={bottomRef} />
-      {isFetchingNextPage && <p>로딩 중...</p>}
-    </div>
-  );
-};
-
 const WritableReviews = () => (
-  <ErrorBoundary fallback={({ error }) => <p>{error.message}</p>}>
-    <Suspense fallback={<ReviewCardSkeleton />}>
-      <SuspenseInfiniteQuery {...infiniteWritableReviewsOptions()}>
-        {(props) => <ReviewList {...props} />}
-      </SuspenseInfiniteQuery>
-    </Suspense>
-  </ErrorBoundary>
+  <InfiniteReviewList<
+    WritableReviewsResponse,
+    WritableReviewsResponse['data'][number]
+  >
+    fallback={<ReviewCardSkeleton />}
+    options={infiniteWritableReviewsOptions(2)}
+    getDataId={(data) => data.groupId}
+    renderData={(data) => (
+      <ReviewCard
+        groupInfo={data}
+        reviewsCount={data.memberCount - data.reviews.length}
+        content={
+          <WritableReviewsContent
+            reviews={data.reviews}
+            groupId={data.groupId}
+          />
+        }
+      />
+    )}
+  />
 );
 
 export default WritableReviews;
--- src/components/pages/reviews/managements/WrittenReviews.tsx
+++ src/components/pages/reviews/managements/WrittenReviews.tsx
@@ -1,60 +1,29 @@
 'use client';
 
 import React from 'react';
-import { ErrorBoundary, Suspense } from '@suspensive/react';
-import { SuspenseInfiniteQuery } from '@suspensive/react-query';
-import {
-  InfiniteData,
-  UseSuspenseInfiniteQueryResult,
-} from '@tanstack/react-query';
+import InfiniteReviewList from '@/components/common/InfiniteReviewList';
 import { infiniteWrittenReviewsOptions } from '@/hooks/reviewHooks/reviewHooks';
-import { useInfiniteScroll } from '@/hooks/useInfiniteScroll/useInfiniteScroll';
-import { ApiResponse } from '@/types/api';
 import { WrittenReviewsResponse } from '@/types/reviews';
 import ReviewCard from './ReviewCard';
 import ReviewCardSkeleton from './ReviewCardSkeleton';
 import WrittenReviewsContent from './WrittenReviewsContent';
 
-const ReviewList = ({
-  data,
-  fetchNextPage,
-  hasNextPage,
-  isFetchingNextPage,
-}: UseSuspenseInfiniteQueryResult<
-  InfiniteData<WrittenReviewsResponse>,
-  ApiResponse
->) => {
-  const bottomRef = useInfiniteScroll<HTMLDivElement>(
-    fetchNextPage,
-    hasNextPage
-  );
-
-  return (
-    <div className='flex flex-col items-center gap-5'>
-      {data.pages.map((page) =>
-        page.data?.map((item) => (
-          <ReviewCard
-            key={item.groupId}
-            groupInfo={item}
-            reviewsCount={item.reviews.length}
-            content={<WrittenReviewsContent reviews={item.reviews} />}
-          />
-        ))
-      )}
-      <div ref={bottomRef} />
-      {isFetchingNextPage && <p>로딩 중...</p>}
-    </div>
-  );
-};
-
 const WrittenReviews = () => (
-  <ErrorBoundary fallback={({ error }) => <p>{error.message}</p>}>
-    <Suspense fallback={<ReviewCardSkeleton />}>
-      <SuspenseInfiniteQuery {...infiniteWrittenReviewsOptions()}>
-        {(props) => <ReviewList {...props} />}
-      </SuspenseInfiniteQuery>
-    </Suspense>
-  </ErrorBoundary>
+  <InfiniteReviewList<
+    WrittenReviewsResponse,
+    WrittenReviewsResponse['data'][number]
+  >
+    fallback={<ReviewCardSkeleton />}
+    options={infiniteWrittenReviewsOptions(2)}
+    getDataId={(data) => data.groupId}
+    renderData={(data) => (
+      <ReviewCard
+        groupInfo={data}
+        reviewsCount={data.reviews.length}
+        content={<WrittenReviewsContent reviews={data.reviews} />}
+      />
+    )}
+  />
 );
 
 export default WrittenReviews;
