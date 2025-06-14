5d4516a82b50b17dd34c5ef3219f92366927117b
Seokyung <xuejd17@gmail.com>
2025-06-14 11:50:29 +0900

    design: ChatMessage에 날짜/시간 추가 및 디자인 임시 적용

--- src/components/pages/groupDetail/Chat/ChatMessage.tsx
+++ src/components/pages/groupDetail/Chat/ChatMessage.tsx
@@ -1,33 +1,42 @@
+import { format } from 'date-fns';
 import ProfileIcon from '@/components/icons/ProfileIcon';
+import { ChatMessage as ChatMessageType } from '@/types/chat';
 
 interface ChatMessageProps {
-  senderName: string;
-  content: string;
+  message: ChatMessageType;
   isMine: boolean;
 }
 
-const ChatMessage = ({ senderName, content, isMine }: ChatMessageProps) => {
-  if (isMine)
+const ChatMessage = ({ message, isMine }: ChatMessageProps) => {
+  if (isMine || message.isMine)
     return (
-      <div className='flex justify-end gap-3.5'>
+      <div className='flex items-end justify-end gap-2'>
+        <span className='text-right text-12_M text-gray-500'>
+          {format(message.createdAt, 'yy.MM.dd hh:mm')}
+        </span>
         <div className='rounded-l-[20px] rounded-tr-[2px] rounded-br-[20px] bg-[#ececec] p-3.5'>
           <span className='text-16_M leading-normal tracking-[-0.4px] text-gray-950'>
-            {content}
+            {message.content}
           </span>
         </div>
       </div>
     );
 
   return (
-    <div className='flex justify-start gap-3.5'>
+    <div className='flex justify-start gap-2'>
       <ProfileIcon className='aspect-square h-10 w-10 shrink-0' />
       <div className='flex flex-col gap-2'>
         <span className='text-14_M leading-normal tracking-[-0.35px] text-gray-950'>
-          {senderName}
+          {message.senderName}
         </span>
-        <div className='rounded-tl-[2px] rounded-r-[20px] rounded-bl-[20px] bg-[#ececec] p-3.5'>
-          <span className='text-16_M leading-normal tracking-[-0.4px] text-gray-950'>
-            {content}
+        <div className='flex items-end gap-2'>
+          <div className='rounded-tl-[2px] rounded-r-[20px] rounded-bl-[20px] bg-[#ececec] p-3.5'>
+            <span className='text-16_M leading-normal tracking-[-0.4px] text-gray-950'>
+              {message.content}
+            </span>
+          </div>
+          <span className='text-left text-12_M text-gray-500'>
+            {format(message.createdAt, 'yy.MM.dd hh:mm')}
           </span>
         </div>
       </div>
