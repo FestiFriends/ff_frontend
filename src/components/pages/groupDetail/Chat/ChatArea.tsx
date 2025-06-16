a0f41d20df1c9de63581358b496e08049f58b728
Seokyung <xuejd17@gmail.com>
2025-06-16 16:44:51 +0900

    feat: ChatArea 컴포넌트 props 추가

--- src/components/pages/groupDetail/Chat/ChatArea.tsx
+++ src/components/pages/groupDetail/Chat/ChatArea.tsx
@@ -1,27 +1,22 @@
 'use client';
 
 import { useChatWebSocket } from '@/hooks/chatHooks/chatHooks';
-import { useGetUserId } from '@/hooks/userHooks/userHooks';
 // import { CHAT_SAMPLE_DATA } from '@/mocks/handlers/chatHandlers';
 import ChatMessageInput from './ChatMessageInput';
 import ChatMessageList from './ChatMessageList';
 
-const chatRoomId = 1;
+interface ChatAreaProps {
+  userId: number | undefined;
+  chatRoomId: number | undefined;
+}
 
-const ChatArea = () => {
-  const { data: userId, isPending } = useGetUserId();
-  const { messages, sendMessage } = useChatWebSocket(
-    userId?.data?.userId,
-    chatRoomId
-  );
-
-  if (isPending) return <div>loading...</div>;
-  if (!isPending && !userId) return <div>접근 권한이 없습니다.</div>;
+const ChatArea = ({ userId, chatRoomId }: ChatAreaProps) => {
+  const { messages, sendMessage } = useChatWebSocket(userId, chatRoomId);
 
   return (
     <div className='relative flex h-[60dvh] flex-col gap-2'>
       <ChatMessageList
-        userId={userId?.data?.userId}
+        userId={userId}
         messages={messages}
         // messages={CHAT_SAMPLE_DATA}
       />
