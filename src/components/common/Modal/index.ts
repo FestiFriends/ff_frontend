afb356f27691f4185843b337a95695e889c435e9
jadewisemann <jadewisemann@gmail.com>
2025-06-16 11:30:40 +0900

    chore: Modal 컴포넌트 re-export

---
+++ src/components/common/Modal/index.ts
@@ -0,0 +1,20 @@
+import Modal from './Modal';
+import ModalAction from './ModalAction';
+import ModalCancel from './ModalCancel';
+import ModalClose from './ModalClose';
+import ModalContent from './ModalContent';
+import { ModalContext, useModalContext } from './ModalContext';
+import ModalTrigger from './ModalTrigger';
+import TwoButtonModal from './TwoButtonModal';
+
+export {
+  Modal,
+  ModalAction,
+  ModalCancel,
+  ModalClose,
+  ModalContent,
+  ModalContext,
+  useModalContext,
+  ModalTrigger,
+  TwoButtonModal,
+};
