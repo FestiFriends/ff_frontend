c21652bb1a597206b97a7abff3c2d19b9a3326f3
songdaramji <songdaramji@gmail.com>
2025-06-17 15:32:32 +0900

    feat: 게시글 카드 props 추가, 모달 추가, 스타일 수정

--- src/components/pages/groupDetail/PostCard/PostCard.tsx
+++ src/components/pages/groupDetail/PostCard/PostCard.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import React from 'react';
-import { format } from 'date-fns';
-import { ko } from 'date-fns/locale';
+import React, { useRef, useState } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import Button from '@/components/common/Button/Button';
+import Modal from '@/components/common/Modal/Modal';
+import ModalAction from '@/components/common/Modal/ModalAction';
+import ModalCancel from '@/components/common/Modal/ModalCancel';
+import ModalContent from '@/components/common/Modal/ModalContent';
+import ModalTrigger from '@/components/common/Modal/ModalTrigger';
 import MoreDropdown from '@/components/common/MoreDropdown/MoreDropdown';
 import ProfileImage from '@/components/common/ProfileImage/ProfileImage';
 import {
@@ -13,18 +18,17 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import { useDeletePost, usePinPost } from '@/hooks/postHooks/postHook';
 import { Post } from '@/types/post';
 
 interface PostCardProps {
   post: Post;
-  children: React.ReactNode;
+  type?: 'posts' | 'detail';
+  children?: React.ReactNode;
 }
 
-const DATE_FORMAT = 'M월 d일 a hh:mm';
-
-const PostCard = ({ post, children }: PostCardProps) => {
+const PostCard = ({ post, type = 'posts', children }: PostCardProps) => {
   const {
-    // id,
     content,
     isPinned = false,
     imageCount,
@@ -32,70 +36,100 @@ const PostCard = ({ post, children }: PostCardProps) => {
     author,
     createdAt,
     isMine,
+    id,
+    groupId,
   } = post;
 
+  const { profileImage, name } = author ?? {};
+  const triggerRef = useRef<HTMLButtonElement>(null);
+  const router = useRouter();
+  const [selectedPostId, setSelectedPostId] = useState<string>('');
+  const [selectedGroupId, setSelectedGroupId] = useState<string>('');
+  const { mutate: deletePost } = useDeletePost();
+  const { mutate: pinPost } = usePinPost();
+
+  const baseItems = isMine
+    ? [
+        {
+          label: '수정하기',
+          onClick: () => router.push(`/groups/${groupId}/posts/${id}/edit`),
+        },
+        {
+          label: '삭제하기',
+          onClick: () => {
+            setSelectedGroupId(groupId);
+            setSelectedPostId(id);
+            triggerRef.current?.click();
+          },
+        },
+      ]
+    : [
+        {
+          label: '신고하기',
+          onClick: () => console.log('신고하기 클릭'),
+        },
+      ];
+
+  const moreDropdownItems = [
+    ...baseItems,
+    isPinned
+      ? {
+          label: '고정 해제',
+          onClick: () => {
+            setSelectedGroupId(groupId);
+            setSelectedPostId(id);
+            pinPost({ groupId, postId: id, isPinned: false });
+          },
+        }
+      : {
+          label: '상단 고정',
+          onClick: () => {
+            setSelectedGroupId(groupId);
+            setSelectedPostId(id);
+            pinPost({ groupId, postId: id, isPinned: true });
+          },
+        },
+  ];
+
   const handlePostClick = () => {
-    console.log('페이지 이동');
+    if (type === 'posts') {
+      router.push(`/groups/${groupId}/posts/${id}`);
+    }
+  };
+
+  const handleModalConfirm = () => {
+    deletePost({ groupId: selectedGroupId, postId: selectedPostId });
   };
 
   return (
-    <div className='w-full px-4'>
-      {/* 카드 테두리 */}
-      <div className='flex w-full flex-col gap-5 rounded-2xl border border-gray-100 bg-white px-6 pt-6 pb-[10px]'>
-        {/* 콘텐츠 영역 */}
+    <div className='w-full'>
+      <div
+        className={`flex w-full flex-col gap-5 bg-white pt-6 ${
+          type === 'posts'
+            ? 'rounded-2xl border border-gray-100 px-6'
+            : 'border-b border-b-gray-100'
+        }`}
+      >
         <div className='flex w-full flex-col gap-3.5'>
-          {/* 게시글 정보 */}
           <div className='relative flex items-start justify-between'>
             <div className='flex items-center gap-2.5'>
               <ProfileImage
                 size='sm'
                 border={false}
-                {...(author.profileImage && { src: author.profileImage })}
+                {...(profileImage
+                  && profileImage.src && { src: profileImage.src.toString() })}
               />
 
               <div className='flex flex-col text-sm'>
-                <span className='text-sm font-bold text-gray-950'>
-                  {author.name}
-                </span>
-                {/* Todo : text 폰트 13이 없어서 커스텀 */}
-                <div className='flex items-center gap-1 text-[13px] font-medium text-gray-500'>
-                  <span>
-                    {format(new Date(createdAt), DATE_FORMAT, {
-                      locale: ko,
-                    })}
-                  </span>
-
+                <span className='text-sm font-bold text-gray-950'>{name}</span>
+                <div className='flex items-center gap-1 text-13_M font-medium text-gray-500'>
+                  <span>{createdAt}</span>
                   {isPinned && <span>· 공지</span>}
                 </div>
               </div>
             </div>
-            <MoreDropdown
-              items={[
-                ...(isMine
-                  ? [
-                      {
-                        label: '수정하기',
-                        onClick: () => console.log('수정하기 클릭'),
-                      },
-                      {
-                        label: '삭제하기',
-                        onClick: () => console.log('삭제하기 클릭'),
-                      },
-                    ]
-                  : [
-                      {
-                        label: '신고하기',
-                        onClick: () => console.log('신고하기 클릭'),
-                      },
-                    ]),
-                {
-                  label: '공지글 등록',
-                  onClick: () => console.log('공지글 등록'),
-                },
-              ]}
-            />
+            <MoreDropdown items={moreDropdownItems} />
           </div>
-          {/* 글, 사진 */}
           <div
             className='flex flex-col gap-5'
             role='button'
@@ -109,7 +143,7 @@ const PostCard = ({ post, children }: PostCardProps) => {
             <div className='text-base whitespace-pre-wrap text-gray-950'>
               {content}
             </div>
-            {imageCount > 1 ? (
+            {imageCount > 1 && type === 'posts' ? (
               <Carousel
                 data-testid='carousel'
                 className='relative mx-auto w-full'
@@ -131,24 +165,52 @@ const PostCard = ({ post, children }: PostCardProps) => {
                 <CarouselPrevious className='left-0.5' />
                 <CarouselNext className='right-0.5' />
               </Carousel>
-            ) : imageCount === 1 && images?.[0]?.src ? (
-              <div className='w-full'>
-                <Image
-                  src={images[0].src}
-                  alt={images[0].alt || '첨부 이미지'}
-                  width={0}
-                  height={0}
-                  sizes='100vw'
-                  className='h-auto w-full object-contain'
-                />
+            ) : (
+              <div className='flex w-full flex-col gap-2.5'>
+                {Array.from(images ?? [], (img) => (
+                  <Image
+                    key={img.id}
+                    src={img.src}
+                    alt={img.alt || '첨부 이미지'}
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    className='h-auto w-full object-contain'
+                  />
+                ))}
               </div>
-            ) : null}
+            )}
           </div>
         </div>
         <div className='flex h-11 items-center justify-between gap-3.5 border-t border-gray-100 text-sm text-gray-500'>
           {children}
         </div>
       </div>
+
+      <Modal>
+        <ModalTrigger>
+          <button ref={triggerRef}></button>
+        </ModalTrigger>
+        <ModalContent className='flex w-[343px] flex-col rounded-2xl bg-white p-5 pt-11'>
+          <p className='mb-[30px] flex w-full justify-center text-16_B text-black'>
+            게시글을 삭제하시겠습니까?
+          </p>
+          <div className='flex w-full justify-between gap-2.5'>
+            <ModalCancel className='w-1/2'>
+              <Button variant='secondary'>아니요</Button>
+            </ModalCancel>
+            <ModalAction className='w-1/2'>
+              <Button
+                onClick={() => {
+                  handleModalConfirm();
+                }}
+              >
+                네
+              </Button>
+            </ModalAction>
+          </div>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
