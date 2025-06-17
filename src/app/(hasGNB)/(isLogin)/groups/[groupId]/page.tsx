36a6fb8cc85c8c28f776ef1ec51b5bce59a00588
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2025-06-17 15:38:20 +0900

    [Feature/issue 356] 모임 상세 페이지 레이아웃 (#361)
    * feat: 모임 상세 페이지 내부 파일 구조 및 기본 레이아웃 설정
    
    * feat: 로그인 여부에 따른 선택적 렌더링 설정

--- src/app/(hasGNB)/(isLogin)/groups/[groupId]/page.tsx
+++ src/app/(hasGNB)/(isLogin)/groups/[groupId]/page.tsx
@@ -1,3 +1,19 @@
-const GroupDetailPage = () => <div>모임 상세 페이지</div>;
+import Header from '@/components/common/Header/Header';
+import GroupWrapper from '@/components/pages/groupDetail/GroupWrapper';
+
+type GroupDetailPageProps = {
+  params: Promise<{ groupId: string }>;
+};
+
+const GroupDetailPage = async ({ params }: GroupDetailPageProps) => {
+  const { groupId } = await params;
+
+  return (
+    <div>
+      <Header />
+      <GroupWrapper groupId={groupId} />
+    </div>
+  );
+};
 
 export default GroupDetailPage;
