75084246d7bd609ef8653fa08ba18307daa89b26
Seokyung <xuejd17@gmail.com>
2025-06-17 15:32:31 +0900

    feat: 로그인 여부에 따른 선택적 렌더링 설정

--- src/components/pages/groupDetail/GroupWrapper.tsx
+++ src/components/pages/groupDetail/GroupWrapper.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useAuthStore } from '@/providers/AuthStoreProvider';
 import GroupInfo from './GroupInfo';
 import GroupMembers from './GroupMembers';
 import GroupTabs from './GroupTabs';
@@ -7,12 +10,20 @@ interface GroupWrapperProps {
 }
 
 const GroupWrapper = ({ groupId }: GroupWrapperProps) => {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedin);
   console.log(groupId); // TODO: 주석 제거 필요
+
   return (
     <div className='flex flex-col gap-7.5 bg-white'>
       <GroupInfo />
-      <GroupMembers />
-      <GroupTabs />
+
+      {/* TODO: isLoggedIn && isMember일 경우에만 렌더링 */}
+      {isLoggedIn && (
+        <>
+          <GroupMembers />
+          <GroupTabs />
+        </>
+      )}
     </div>
   );
 };
