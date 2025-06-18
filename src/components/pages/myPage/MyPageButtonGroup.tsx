ca196eb2182818b5516a595ab7e6988cb6a68a5d
CHAN-H0 <lch0309k@gmail.com>
2025-06-18 15:18:16 +0900

    fix: 마이페이지 디테일 수정

--- src/components/pages/myPage/MyPageButtonGroup.tsx
+++ src/components/pages/myPage/MyPageButtonGroup.tsx
@@ -11,7 +11,7 @@ const MyPageButtonGroup = () => {
     <div className='mt-[30px] mb-[24px] grid w-full max-w-md grid-cols-2 gap-[14px]'>
       <button
         onClick={() => router.push('/mypage/liked-performances')}
-        className='flex flex-col items-center justify-center rounded-[12px] bg-gray-25 px-[30px] py-[20px]'
+        className='flex h-[86px] flex-col items-center justify-center rounded-[12px] bg-gray-25 px-[30px] py-[20px]'
       >
         <NoteIcon className='text-gray-400' />
         <span className='mt-[8px] text-14_B font-medium text-gray-950'>
@@ -20,7 +20,7 @@ const MyPageButtonGroup = () => {
       </button>
       <button
         onClick={() => router.push('/mypage/liked-users')}
-        className='flex flex-col items-center justify-center rounded-[12px] bg-gray-25 px-[30px] py-[20px]'
+        className='flex h-[86px] flex-col items-center justify-center rounded-[12px] bg-gray-25 px-[30px] py-[20px]'
       >
         <UserIcon className='text-gray-400' />
         <span className='mt-[8px] text-14_B font-medium text-gray-950'>
