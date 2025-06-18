562b9ebcee4be5d92fcb7cea44cf7496f447601e
songdaramji <songdaramji@gmail.com>
2025-06-18 21:24:25 +0900

    feat: 수정 아이콘 추가, X아이콘 수정

---
+++ src/components/icons/EditIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const EditIcon = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+  >
+    <path
+      d='M16.0833 2.01172L19.0833 5.01172L16.7963 7.29972L13.7963 4.29972L16.0833 2.01172ZM4.08325 13.9997V16.9997H7.08325L15.3823 8.71272L12.3823 5.71272L4.08325 13.9997ZM4.08325 19.9997H20.0833V21.9997H4.08325V19.9997Z'
+      fill='white'
+    />
+  </svg>
+);
+
+export default EditIcon;
