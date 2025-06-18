aa33fe9a7309e5e201cd998b0030fe3b7339b32c
SeongIn Ko <jjjk0605@naver.com>
2025-06-18 16:43:41 +0900

    style: ModalContent h-full 삭제

--- src/components/common/Modal/ModalContent.tsx
+++ src/components/common/Modal/ModalContent.tsx
@@ -77,7 +77,7 @@ const ModalContent = ({
         className={cn(
           restoreDialogStyle
             ? ''
-            : 'fixed top-1/2 left-1/2 h-full -translate-x-1/2 -translate-y-1/2',
+            : 'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
           className
         )}
         onClose={handleClose}
