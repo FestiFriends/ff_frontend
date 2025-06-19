8b3c384e81766c6afbeda82deabbde7d0930d08e
Seokyung <xuejd17@gmail.com>
2025-06-19 12:50:16 +0900

    feat: 역할이 HOST인 사람만 권한 수정/퇴출 버튼 보이도록 처리

--- src/components/pages/groupDetail/Members/MemberCard.tsx
+++ src/components/pages/groupDetail/Members/MemberCard.tsx
@@ -1,6 +1,7 @@
 import { CrownIcon } from 'lucide-react';
 import ProfileImage from '@/components/common/ProfileImage/ProfileImage';
 import { RoleLabels } from '@/constants/roleLabels';
+import { useAuthStore } from '@/providers/AuthStoreProvider';
 import { RoleType } from '@/types/enums';
 import { Member } from '@/types/group';
 import MemberDeleteModal from './MemberDeleteModal';
@@ -8,39 +9,46 @@ import MemberRoleModal from './MemberRoleModal';
 
 interface MemberCardProps {
   member: Member;
+  isHost?: boolean;
 }
 
-const MemberCard = ({ member }: MemberCardProps) => (
-  <div
-    key={member.memberId}
-    className='flex items-center justify-between rounded-[16px] border-1 border-gray-100 bg-white p-3.5'
-  >
-    <div className='flex items-center justify-center gap-3'>
-      <ProfileImage
-        size='md'
-        src={member.profileImage}
-        alt={member.name}
-        border={false}
-        className='aspect-square shrink-0'
-      />
-      <div className='flex flex-col justify-center gap-0.5'>
-        <span className='flex items-center text-center text-12_M tracking-[-0.35px] text-gray-500'>
-          {member.role === 'HOST' && (
-            <CrownIcon className='mr-[1px] aspect-square h-3 w-3' />
-          )}
-          {RoleLabels[member.role as RoleType]}
-        </span>
-        <span className='text-center text-16_B leading-normal tracking-[-0.4px] text-gray-950'>
-          {member.name}
-        </span>
-      </div>
-    </div>
+const MemberCard = ({ member, isHost }: MemberCardProps) => {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedin);
 
-    <div className='flex gap-3'>
-      <MemberRoleModal member={member} />
-      <MemberDeleteModal member={member} />
+  return (
+    <div
+      key={member.memberId}
+      className='flex items-center justify-between rounded-[16px] border-1 border-gray-100 bg-white p-3.5'
+    >
+      <div className='flex items-center justify-center gap-3'>
+        <ProfileImage
+          size='md'
+          src={member.profileImage}
+          alt={member.name}
+          border={false}
+          className='aspect-square shrink-0'
+        />
+        <div className='flex flex-col justify-center gap-0.5'>
+          <span className='ml-[0.5px] flex items-center text-center text-12_M tracking-[-0.35px] text-gray-500'>
+            {member.role === 'HOST' && (
+              <CrownIcon className='mr-[1px] aspect-square h-3 w-3' />
+            )}
+            {RoleLabels[member.role as RoleType]}
+          </span>
+          <span className='text-center text-16_B leading-normal tracking-[-0.4px] text-gray-950'>
+            {member.name}
+          </span>
+        </div>
+      </div>
+
+      {isHost && member.role !== 'HOST' && isLoggedIn && (
+        <div className='flex gap-3'>
+          <MemberRoleModal member={member} />
+          <MemberDeleteModal member={member} />
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default MemberCard;
