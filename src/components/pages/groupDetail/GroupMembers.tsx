39f28de37ddec24ea835070d67a79df0287e1430
Seokyung <xuejd17@gmail.com>
2025-06-19 12:49:10 +0900

    fix: Carousel 모임원 목록 조회 호출 위치 변경

--- src/components/pages/groupDetail/GroupMembers.tsx
+++ src/components/pages/groupDetail/GroupMembers.tsx
@@ -1,3 +1,4 @@
+import { useGetGroupMembers } from '@/hooks/groupHooks/groupHooks';
 import MembersHeader from './Members/MembersHeader';
 import MembersList from './Members/MembersList';
 
@@ -5,11 +6,27 @@ interface GroupMembersProps {
   groupId: string;
 }
 
-const GroupMembers = ({ groupId }: GroupMembersProps) => (
-  <div className='flex flex-col gap-5 px-4'>
-    <MembersHeader groupId={groupId} />
-    <MembersList groupId={groupId} />
-  </div>
-);
+const GroupMembers = ({ groupId }: GroupMembersProps) => {
+  const {
+    data: groupMembers,
+    isPending,
+    isError,
+  } = useGetGroupMembers(groupId, 5);
+
+  return (
+    <div className='flex flex-col gap-5 px-4'>
+      <MembersHeader
+        groupId={groupId}
+        memberCount={groupMembers?.data.memberCount}
+        isHost={groupMembers?.data.isHost}
+      />
+      <MembersList
+        members={groupMembers?.data.members}
+        isPending={isPending}
+        isError={isError}
+      />
+    </div>
+  );
+};
 
 export default GroupMembers;
