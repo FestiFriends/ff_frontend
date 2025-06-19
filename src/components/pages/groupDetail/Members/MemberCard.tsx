bc826bee5525ea2dd4c45910b9a72d12994bb756
Seokyung <xuejd17@gmail.com>
2025-06-19 12:58:13 +0900

    feat: 프로필 이미지나 이름 클릭 시 프로필 페이지로 라우팅

--- src/components/pages/groupDetail/Members/MemberCard.tsx
+++ src/components/pages/groupDetail/Members/MemberCard.tsx
@@ -1,4 +1,5 @@
 import { CrownIcon } from 'lucide-react';
+import Link from 'next/link';
 import ProfileImage from '@/components/common/ProfileImage/ProfileImage';
 import { RoleLabels } from '@/constants/roleLabels';
 import { useAuthStore } from '@/providers/AuthStoreProvider';
@@ -20,14 +21,16 @@ const MemberCard = ({ member, isHost }: MemberCardProps) => {
       key={member.memberId}
       className='flex items-center justify-between rounded-[16px] border-1 border-gray-100 bg-white p-3.5'
     >
-      <div className='flex items-center justify-center gap-3'>
-        <ProfileImage
-          size='md'
-          src={member.profileImage}
-          alt={member.name}
-          border={false}
-          className='aspect-square shrink-0'
-        />
+      <div className='flex items-center justify-start gap-3'>
+        <Link href={`/profiles/${member.memberId}`}>
+          <ProfileImage
+            size='md'
+            src={member.profileImage}
+            alt={member.name}
+            border={false}
+            className='aspect-square shrink-0'
+          />
+        </Link>
         <div className='flex flex-col justify-center gap-0.5'>
           <span className='ml-[0.5px] flex items-center text-center text-12_M tracking-[-0.35px] text-gray-500'>
             {member.role === 'HOST' && (
@@ -35,9 +38,12 @@ const MemberCard = ({ member, isHost }: MemberCardProps) => {
             )}
             {RoleLabels[member.role as RoleType]}
           </span>
-          <span className='text-center text-16_B leading-normal tracking-[-0.4px] text-gray-950'>
+          <Link
+            href={`/profiles/${member.memberId}`}
+            className='text-center text-16_B leading-normal tracking-[-0.4px] text-gray-950'
+          >
             {member.name}
-          </span>
+          </Link>
         </div>
       </div>
 
