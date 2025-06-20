e0886b04bd44c4e5b81035c7940b190dcb70982f
jadewisemann <jadewisemann@gmail.com>
2025-06-20 17:07:12 +0900

    refactor: ExpiredButton 로직 수정

--- src/components/pages/performances/IsExpiredButton.tsx
+++ src/components/pages/performances/IsExpiredButton.tsx
@@ -1,27 +1,23 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import { useQueryParam } from '@/hooks';
 import { cn } from '@/lib/utils';
 
 const IsExpiredButton = () => {
   const { getQueryParam, setQueryParam } = useQueryParam();
-  const [isExpired, setIsExpired] = useState(() => getQueryParam('isExpired'));
+  const queryValue = getQueryParam('isExpired');
+  const isExpired = queryValue !== 'false';
 
   const handleClick = () => {
-    if (isExpired === 'false') {
-      setIsExpired('true');
-      setQueryParam('isExpired', 'true');
-    } else if (isExpired === 'true') {
-      setIsExpired('false');
-      setQueryParam('isExpired', 'false');
-    }
+    const newValue = !isExpired;
+    setQueryParam('isExpired', newValue ? 'true' : 'false');
   };
   return (
     <button
       onClick={handleClick}
       className={cn(
         'inline-flex cursor-pointer items-center justify-center gap-1 rounded-[100px] border-1 border-gray-100 bg-white py-3 pr-4 pl-5 whitespace-nowrap transition-all select-none',
-        isExpired === 'true' && 'bg-gray-950 text-white'
+        isExpired && 'bg-gray-950 text-white'
       )}
     >
       <span className={'text-14_M'}>지난 공연 보기</span>
