8f76844144ac2ff679b392352134cf1ad8f35283
jadewisemann <jadewisemann@gmail.com>
2025-06-20 15:44:57 +0900

    choer: 하드 코딩된 label, enum 사용

--- src/components/pages/myPage/MyPageButtonGroup.tsx
+++ src/components/pages/myPage/MyPageButtonGroup.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation';
 import NoteIcon from '@/components/icons/NoteIcon';
 import UserIcon from '@/components/icons/UserIcon';
+import { favoriteTabLabel } from '@/types/tabs';
 
 const MyPageButtonGroup = () => {
   const router = useRouter();
@@ -10,21 +11,23 @@ const MyPageButtonGroup = () => {
   return (
     <div className='mt-[30px] mb-[24px] grid w-full max-w-md grid-cols-2 gap-[14px]'>
       <button
-        onClick={() => router.push('/favorite?tab=공연')}
-        className='flex h-[86px] flex-col items-center justify-center rounded-[12px] bg-gray-25 px-[30px] py-[20px]'
-      >
-        <NoteIcon className='text-gray-400' />
-        <span className='mt-[8px] text-14_B font-medium text-gray-950'>
-          찜한 공연
-        </span>
-      </button>
-      <button
-        onClick={() => router.push('/favorite?tab=사용자')}
+        onClick={() => router.push('/favorite?tab=' + favoriteTabLabel.USERS)}
         className='flex h-[86px] flex-col items-center justify-center rounded-[12px] bg-gray-25 px-[30px] py-[20px]'
       >
         <UserIcon className='text-gray-400' />
         <span className='mt-[8px] text-14_B font-medium text-gray-950'>
-          찜한 유저
+          {favoriteTabLabel.USERS}
+        </span>
+      </button>
+      <button
+        onClick={() =>
+          router.push('/favorite?tab=' + favoriteTabLabel.PERFORMANCES)
+        }
+        className='flex h-[86px] flex-col items-center justify-center rounded-[12px] bg-gray-25 px-[30px] py-[20px]'
+      >
+        <NoteIcon className='text-gray-400' />
+        <span className='mt-[8px] text-14_B font-medium text-gray-950'>
+          {favoriteTabLabel.PERFORMANCES}
         </span>
       </button>
     </div>
