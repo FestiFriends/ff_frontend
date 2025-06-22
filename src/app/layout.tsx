37c52c4cbbb175de4cf67d46a87edcc6dfb75944
Seokyung <xuejd17@gmail.com>
2025-06-23 00:48:47 +0900

    chore: meta description 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,17 +10,22 @@ import { SseStoreProvider } from '@/providers/SseStoreProvider';
 
 initMocks();
 
+const SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL ?? 'https://ff-frontend-rust.vercel.app';
+
 export const metadata: Metadata = {
-  title: 'FestiFriends - 숙소부터 공연장까지 함께할 덕메 찾기',
-  description: '숙소부터 공연장까지 함께할 친구들을 찾아보세요',
+  title: 'FestiFriends - 숙소부터 공연장까지 함께할 친구 찾기',
+  description:
+    '공연·페스티벌 숙소부터 관람까지 함께할 친구들을 쉽고 빠르게 찾아보세요. FestiFriends에서 오늘의 공연을 확인하고 모임을 만들어보세요!',
   openGraph: {
-    title: 'FestiFriends - 숙소부터 공연장까지 함께할 덕메 찾기',
-    description: '숙소부터 공연장까지 함께할 친구들을 찾아보세요',
-    url: process.env.NEXT_PUBLIC_SITE_URL,
+    title: 'FestiFriends - 숙소부터 공연장까지 함께할 친구 찾기',
+    description:
+      '공연·페스티벌 숙소부터 관람까지 함께할 친구들을 쉽고 빠르게 찾아보세요.',
+    url: SITE_URL,
     siteName: 'FestiFriends',
     images: [
       {
-        url: `${process.env.NEXT_PUBLIC_SITE_URL}/og-image.jpg`,
+        url: `${SITE_URL}/og-image.jpg`,
         width: 1200,
         height: 630,
         alt: 'FestiFriends 오픈그래프 이미지',
@@ -31,9 +36,10 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'FestiFriends - 숙소부터 공연장까지 함께할 덕메 찾기',
-    description: '숙소부터 공연장까지 함께할 친구들을 찾아보세요',
-    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/og-image.jpg`],
+    title: 'FestiFriends - 숙소부터 공연장까지 함께할 친구 찾기',
+    description:
+      '공연·페스티벌 숙소부터 관람까지 함께할 친구들을 쉽고 빠르게 찾아보세요.',
+    images: [`${SITE_URL}/og-image.jpg`],
   },
   icons: {
     icon: [
