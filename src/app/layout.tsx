7d125d53bbc3f4f0db5b8a481a61a51e0588d418
Seokyung <xuejd17@gmail.com>
2025-06-22 09:27:12 +0900

    chore: 기본 메타 태그 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import TabBar from '@/components/common/TabBar/TabBar';
 import NotificationToast from '@/components/pages/NotificationToast';
 import { initMocks } from '@/mocks';
@@ -8,6 +9,44 @@ import './globals.css';
 import { SseStoreProvider } from '@/providers/SseStoreProvider';
 
 initMocks();
+
+export const metadata: Metadata = {
+  title: 'FestiFriends - 숙소부터 공연장까지 함께할 덕메 찾기',
+  description: '숙소부터 공연장까지 함께할 친구들을 찾아보세요',
+  openGraph: {
+    title: 'FestiFriends - 숙소부터 공연장까지 함께할 덕메 찾기',
+    description: '숙소부터 공연장까지 함께할 친구들을 찾아보세요',
+    url: process.env.NEXT_PUBLIC_SITE_URL,
+    siteName: 'FestiFriends',
+    // TODO: openGraph용 이미지 생성 후 적용
+    // images: [
+    //   {
+    //     url: `${process.env.NEXT_PUBLIC_SITE_URL}/og-image.png`,
+    //     width: 1200,
+    //     height: 630,
+    //     alt: 'FestiFriends 로고 이미지',
+    //   },
+    // ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'FestiFriends - 숙소부터 공연장까지 함께할 덕메 찾기',
+    description: '숙소부터 공연장까지 함께할 친구들을 찾아보세요',
+    // TODO: openGraph용 이미지 생성 후 적용
+    // images: [`${process.env.NEXT_PUBLIC_SITE_URL}/twitter-image.png`],
+  },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+};
 
 const RootLayout = ({
   children,
