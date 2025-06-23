c85fd4a30f4e15fcd776191d4df0718057f972f4
Seokyung <xuejd17@gmail.com>
2025-06-23 15:22:20 +0900

    fix: 일정 조회 api 로직 수정

--- src/hooks/useSchedules/useSchedules.ts
+++ src/hooks/useSchedules/useSchedules.ts
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
+import { formatISO } from 'date-fns';
 import { GROUP_QUERY_KEYS } from '@/constants/queryKeys';
 import { groupsApi } from '@/services/groupsService';
 import { GroupSchedule } from '@/types/group';
 
 export const useSchedules = (groupId: string, start: Date, end: Date) => {
   const startDate = start.toISOString();
-  const endDate = end.toISOString();
+  const endDate = formatISO(end.valueOf() + 9 * 60 * 60 * 1000);
 
   return useQuery<GroupSchedule[], Error>({
     queryKey: [GROUP_QUERY_KEYS.schedules, groupId, startDate, endDate],
