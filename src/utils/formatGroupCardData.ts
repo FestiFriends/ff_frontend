618259fe1b6ae1f763e1c1b2f1a81229a9b1956c
songdaramji <songdaramji@gmail.com>
2025-06-23 10:26:06 +0900

    fix: split('T')로 날짜 포맷팅 수정

--- src/utils/formatGroupCardData.ts
+++ src/utils/formatGroupCardData.ts
@@ -1,6 +1,7 @@
 import { CursorResponse, PageResponse } from '@/types/api';
 import { GenderType, GroupCategoryType } from '@/types/enums';
 import { Group } from '@/types/group';
+import { formatNormalDate } from './date';
 
 export interface PerformanceGroupsApiResponse {
   code: number;
@@ -108,8 +109,8 @@ export const formatJoinedGroups = (groups: JoinedGroupsApiResponse): Group[] =>
     startAge: group.startAge,
     endAge: group.endAge,
     location: group.location,
-    startDate: group.startDate,
-    endDate: group.endDate,
+    startDate: formatNormalDate(group.startDate),
+    endDate: formatNormalDate(group.endDate),
     memberCount: group.memberCount,
     maxMembers: group.maxMembers,
     description: group.description,
