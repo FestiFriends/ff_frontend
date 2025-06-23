c8f5a07f68cdc18eb285a5b2eb3f3cb3c435a883
Seokyung <xuejd17@gmail.com>
2025-06-24 03:41:12 +0900

    fix: 작성한 리뷰 모달에서 텍스트 리뷰도 볼 수 있게 수정

--- src/components/pages/reviews/managements/WrittenReviewsModal.tsx
+++ src/components/pages/reviews/managements/WrittenReviewsModal.tsx
@@ -37,16 +37,26 @@ const WrittenReviewsModal = ({ review }: WrittenReviewsModalProps) => (
             </span>
           </div>
 
-          <div className='flex flex-col gap-[14px]'>
-            {review.defaultTag.map((tag) => (
-              <span
-                key={tag}
-                className='flex h-10 w-full items-center rounded-[6px] bg-[#f4f4f4] px-4 text-14_M'
-              >
-                {ReviewTagLabels[tag]}
+          {review.defaultTag.length > 0 && (
+            <div className='flex flex-col gap-[14px]'>
+              {review.defaultTag.map((tag) => (
+                <span
+                  key={tag}
+                  className='flex w-full items-center rounded-[6px] bg-[#f4f4f4] px-4 py-3 text-14_M leading-normal tracking-[-0.35px]'
+                >
+                  {ReviewTagLabels[tag]}
+                </span>
+              ))}
+            </div>
+          )}
+
+          {review.content !== '' && (
+            <div>
+              <span className='flex w-full items-center rounded-[6px] bg-[#f4f4f4] px-4 py-3 text-14_M leading-normal tracking-[-0.35px]'>
+                {review.content}
               </span>
-            ))}
-          </div>
+            </div>
+          )}
 
           <div className='flex flex-col'>
             <span className='text-12_B text-gray-500'>별점</span>
