e5ba78cc8847e1cfdc0ebec0f320c779747b51fa
jadewisemann <jadewisemann@gmail.com>
2025-06-23 22:39:27 +0900

    refactor: 각 컴포넌트에 TwoButton 도입

--- src/components/pages/createGroup/FormActions.tsx
+++ src/components/pages/createGroup/FormActions.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, TwoButtonModal, Toast } from '@/components/common';
+import { TwoButton, TwoButtonModal, Toast } from '@/components/common';
 import { useModalController } from '@/hooks';
 
 interface FormActionProps {
@@ -40,25 +40,16 @@ const FormAction = ({
 
   return (
     <>
-      <div className='flex gap-4'>
-        <Button
-          type='button'
-          onClick={() => resetModal.openModal()}
-          variant='secondary'
-          className='flex-1 whitespace-nowrap'
-        >
-          초기화
-        </Button>
-        <Button
-          type='submit'
-          onClick={onSubmit}
-          disabled={!isValid || isSubmitting}
-          variant='primary'
-          className='flex-1 whitespace-nowrap disabled:cursor-not-allowed'
-        >
-          {isSubmitting ? '생성 중...' : '모임 만들기'}
-        </Button>
-      </div>
+      <TwoButton
+        leftText='초기화'
+        rightText={isSubmitting ? '생성 중...' : '모임 만들기'}
+        leftAction={() => resetModal.openModal()}
+        rightAction={onSubmit}
+        leftVariant='secondary'
+        rightVariant='primary'
+        rightDisabled={!isValid || isSubmitting}
+        rightClassName='disabled:cursor-not-allowed'
+      />
 
       <TwoButtonModal
         isOpen={resetModal.isOpen}
