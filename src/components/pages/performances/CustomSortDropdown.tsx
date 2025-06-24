1f90f6125471e4dd385c3ba13ebb99500e9f7278
SeongIn Ko <jjjk0605@naver.com>
2025-06-24 10:06:29 +0900

    fix: 사파리 브라우저에서 공연 목록 필터들이 가려지는 오류 수정

--- src/components/pages/performances/CustomSortDropdown.tsx
+++ src/components/pages/performances/CustomSortDropdown.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState, useRef } from 'react';
+import { createPortal } from 'react-dom';
 import { AltArrowUpIcon, DeleteIcon } from '@/components/icons';
 import { useClickOutside, useQueryParam } from '@/hooks/';
 import { cn } from '@/lib/utils';
@@ -113,32 +114,42 @@ const CustomSortDropdown = ({
   );
 
   return (
-    <div
-      className={cn('relative inline-block rounded-[100px]', className)}
-      ref={dropdownRef}
-    >
-      <TriggerButton />
-      {isOpen && (
-        <div
-          className={cn(
-            'fixed z-20 mt-2 flex flex-col overflow-hidden rounded-[12px] border border-gray-50 bg-white shadow-lg',
-            {
-              'w-full': width === 'full',
-              'w-auto': width === 'auto',
-              'w-fit min-w-25': width === 'fit',
-            }
-          )}
-        >
-          {options.map((option, index) => (
-            <OptionButton
-              option={option}
-              index={index}
-              key={option.value}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <div
+        className={cn('relative inline-block rounded-[100px]', className)}
+        ref={dropdownRef}
+      >
+        <TriggerButton />
+      </div>
+      {isOpen
+        && typeof window !== 'undefined'
+        && dropdownRef.current
+        && createPortal(
+          <div
+            className={cn(
+              'fixed z-20 mt-2 flex flex-col overflow-hidden rounded-[12px] border border-gray-50 bg-white shadow-lg',
+              {
+                'w-full': width === 'full',
+                'w-auto': width === 'auto',
+                'w-fit min-w-25': width === 'fit',
+              }
+            )}
+            style={{
+              top: dropdownRef.current.getBoundingClientRect().bottom + 8,
+              left: dropdownRef.current.getBoundingClientRect().left,
+            }}
+          >
+            {options.map((option, index) => (
+              <OptionButton
+                option={option}
+                index={index}
+                key={option.value}
+              />
+            ))}
+          </div>,
+          document.body
+        )}
+    </>
   );
 };
 
