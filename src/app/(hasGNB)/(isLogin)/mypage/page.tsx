cf5e2d2d73b38bc5ba708cc3fc349fde568b4e63
SeongIn Ko <jjjk0605@naver.com>
2025-06-24 11:27:09 +0900

    fix: 마이페이지 스크롤 수정

--- src/app/(hasGNB)/(isLogin)/mypage/page.tsx
+++ src/app/(hasGNB)/(isLogin)/mypage/page.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@/components/common';
 import MyPageMain from '@/components/pages/myPage/myPageMain';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const MyProfilePage = () => (
   <>
     <Header title='마이페이지' />
-    <MyPageMain />
+    <ScrollArea className='h-[calc(100dvh-124px)]'>
+      <div className='w-screen max-w-lg'>
+        <MyPageMain />
+      </div>
+    </ScrollArea>
   </>
 );
 
